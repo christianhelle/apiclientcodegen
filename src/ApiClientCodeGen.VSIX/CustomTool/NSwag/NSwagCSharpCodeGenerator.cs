44455446699ce425eacfdc64163dd2ebfe6bdeca
Christian Helle <christian.helle@outlook.com>
2019-01-22 23:36:28 +0100

    Implement NSwag single file code generator

---
+++ src/ApiClientCodeGen.VSIX/CustomTool/NSwag/NSwagCSharpCodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.NSwag
+{
+    [Guid("0A31911A-4D1B-47CB-8F89-B93731A1FA31")]
+    [ComVisible(true)]
+    [ProvideObject(typeof(NSwagCSharpCodeGenerator))]
+    [CodeGeneratorRegistration(typeof(NSwagCSharpCodeGenerator),
+                              Description,
+                              ProvideCodeGeneratorAttribute.CSharpProjectGuid,
+                              GeneratesDesignTimeSource = true,
+                              GeneratorRegKeyName = "NSwagCodeGenerator")]
+    public class NSwagCSharpCodeGenerator : NSwagCodeGenerator
+    {
+        public const string Description = "C# NSwag API Client Code Generator";
+
+        public NSwagCSharpCodeGenerator() 
+            : base(SupportedLanguage.CSharp)
+        {
+        }
+
+        public override int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".cs";
+            return 0;
+        }
+    }
+}
---
+++ src/ApiClientCodeGen.VSIX/CustomTool/NSwag/NSwagCodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using System.Runtime.InteropServices;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.NSwag
+{
+    [ComVisible(true)]
+    public abstract class NSwagCodeGenerator : SingleFileCodeGenerator
+    {
+        protected NSwagCodeGenerator(SupportedLanguage language)
+            : base(SupportedCodeGenerator.NSwag, language)
+        {
+        }
+    }
+}
