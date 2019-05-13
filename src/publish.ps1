3b84b4c5bf5fd4abbd53f454735352b4425fb6d5
Christian Helle <christian.helle@outlook.com>
2019-05-13 11:48:30 +0200

    Create deployment script

---
+++ src/publish.ps1
@@ -0,0 +1,17 @@
+$VisualStudioVersion = "15.0";
+$VSINSTALLDIR =  $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion;
+$VSIXPublisherPath = $VSINSTALLDIR + "VSSDK\VisualStudioIntegration\Tools\Bin\"
+$env:Path += ";$VSIXPublisherPath"
+
+$DropFolder = "$(System.DefaultWorkingDirectory)/CI Builds/drop"
+$VSIXFileName = $DropFolder + "/" + (Get-ChildItem -Path $DropFolder -Filter "ApiClientCodeGenerator-*.vsix" | Select-Object -First 1).Name
+$ManifestFile = $DropFolder + "/publish-manifest.json"
+
+VsixPublisher.exe login `
+    -personalAccessToken '$(PersonalAccessToken)' `
+    -publisherName 'ChristianResmaHelle'
+
+VsixPublisher.exe publish `
+    -payload $VSIXFileName `
+    -publishManifest $ManifestFile `
+    -ignoreWarnings 'VSIXValidatorWarning01,VSIXValidatorWarning02'
