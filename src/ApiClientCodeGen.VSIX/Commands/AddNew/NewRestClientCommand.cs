1963a10deabb9593be6e9a4464b760b2db6ef81d
Christian Resma Helle <christian.helle@outlook.com>
2019-05-27 00:22:07 +0200

    Implement Add New API Client from OpenAPI Specification context menu

--- src/ApiClientCodeGen.VSIX/Commands/AddNew/NewRestClientCommand.cs
+++ src/ApiClientCodeGen.VSIX/Commands/AddNew/NewRestClientCommand.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Utility;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Views;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Threading;
 using Task = System.Threading.Tasks.Task;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Commands.AddNew
@@ -19,17 +21,17 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Commands.AddNew
 
         protected int CommandId { get; } = 0x100;
         protected Guid CommandSet { get; } = new Guid("E4B99F94-D11F-4CAA-ADCD-24302C232938");
-        
+
         private DTE _dte;
 
         public async Task InitializeAsync(AsyncPackage package, CancellationToken token)
         {
             await package.JoinableTaskFactory.SwitchToMainThreadAsync(token);
-            
+
             var dteTask = package.GetServiceAsync(typeof(DTE));
             if (dteTask == null)
                 return;
-            
+
             _dte = await dteTask as DTE;
             if (_dte == null)
                 return;
@@ -49,6 +51,44 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Commands.AddNew
 
         private void OnExecute(object sender, EventArgs e)
         {
+            var result = EnterOpenApiSpecDialog.GetResult();
+            if (result == null)
+                return;
+
+            var folder = FindFolder(ProjectHelpers.GetSelectedItem());
+            if (string.IsNullOrWhiteSpace(folder))
+            {
+                Trace.WriteLine("Unable to get folder name");
+                return;
+            }
+
+            var filePath = Path.Combine(folder, result.OutputFilename);
+            File.WriteAllText(filePath, result.OpenApiSpecification);
+
+            var fileInfo = new FileInfo(filePath);
+            var project = ProjectHelpers.GetActiveProject();
+            var projectItem = project.AddFileToProject(fileInfo);
+
+            var customTool = result.SelectedCodeGenerator.GetCustomToolName();
+            projectItem.Properties.Item("CustomTool").Value = customTool;
+        }
+
+        private static string FindFolder(object item)
+        {
+            switch (item)
+            {
+                case ProjectItem projectItem:
+                {
+                    var fileName = projectItem.FileNames[1];
+                    return File.Exists(fileName) ? Path.GetDirectoryName(fileName) : fileName;
+                }
+
+                case Project project:
+                    return project.GetRootFolder();
+
+                default:
+                    return null;
+            }
         }
     }
 }
