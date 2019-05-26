df862841e1d9da21449fcf5eecb5845395bcbd92
Christian Resma Helle <christian.helle@outlook.com>
2019-05-27 00:06:05 +0200

    Add ProjectHelpers utility class

---
+++ src/ApiClientCodeGen.VSIX/Utility/ProjectHelpers.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Utility
+{
+    public static class ProjectHelpers
+    {
+        private static readonly DTE Dte = VsPackage.Dte;
+
+        public static string GetRootNamespace(this Project project)
+        {
+            if (project == null)
+                return null;
+
+            var ns = project.Name ?? string.Empty;
+
+            try
+            {
+                var prop = project.Properties.Item("RootNamespace");
+
+                if (prop != null && prop.Value != null && !string.IsNullOrEmpty(prop.Value.ToString()))
+                    ns = prop.Value.ToString();
+            }
+            catch
+            {
+                /* Project doesn't have a root namespace */
+            }
+
+            return CleanNameSpace(ns, false);
+        }
+
+        public static string CleanNameSpace(string ns, bool stripPeriods = true)
+        {
+            if (stripPeriods) ns = ns.Replace(".", "");
+
+            ns = ns.Replace(" ", "")
+                .Replace("-", "")
+                .Replace("\\", ".");
+
+            return ns;
+        }
+
+        public static string GetRootFolder(this Project project)
+        {
+            if (project == null)
+                return null;
+
+            if (project.IsKind("{66A26720-8FB5-11D2-AA7E-00C04F688DDE}")) //ProjectKinds.vsProjectKindSolutionFolder
+                return Path.GetDirectoryName(Dte.Solution.FullName);
+
+            if (string.IsNullOrEmpty(project.FullName))
+                return null;
+
+            string fullPath;
+
+            try
+            {
+                fullPath = project.Properties.Item("FullPath").Value as string;
+            }
+            catch (ArgumentException)
+            {
+                try
+                {
+                    // MFC projects don't have FullPath, and there seems to be no way to query existence
+                    fullPath = project.Properties.Item("ProjectDirectory").Value as string;
+                }
+                catch (ArgumentException)
+                {
+                    // Installer projects have a ProjectPath.
+                    fullPath = project.Properties.Item("ProjectPath").Value as string;
+                }
+            }
+
+            if (string.IsNullOrEmpty(fullPath))
+                return File.Exists(project.FullName) ? Path.GetDirectoryName(project.FullName) : null;
+
+            if (Directory.Exists(fullPath))
+                return fullPath;
+
+            if (File.Exists(fullPath))
+                return Path.GetDirectoryName(fullPath);
+
+            return null;
+        }
+
+        public static ProjectItem AddFileToProject(this Project project, FileInfo file, string itemType = null)
+        {
+            if (project.IsKind(ProjectTypes.ASPNET_5, ProjectTypes.SSDT))
+                return Dte.Solution.FindProjectItem(file.FullName);
+
+            var root = project.GetRootFolder();
+
+            if (string.IsNullOrEmpty(root) || !file.FullName.StartsWith(root, StringComparison.OrdinalIgnoreCase))
+                return null;
+
+            var item = project.ProjectItems.AddFromFile(file.FullName);
+            item.SetItemType(itemType);
+            return item;
+        }
+
+        public static void SetItemType(this ProjectItem item, string itemType)
+        {
+            try
+            {
+                if (item == null || item.ContainingProject == null)
+                    return;
+
+                if (string.IsNullOrEmpty(itemType) ||
+                    item.ContainingProject.IsKind(ProjectTypes.WEBSITE_PROJECT) ||
+                    item.ContainingProject.IsKind(ProjectTypes.UNIVERSAL_APP))
+                    return;
+
+                item.Properties.Item("ItemType").Value = itemType;
+            }
+            catch (Exception ex)
+            {
+                OutputWindow.Log(ex);
+            }
+        }
+
+        public static bool IsKind(this Project project, params string[] kindGuids)
+        {
+            foreach (var guid in kindGuids)
+                if (project.Kind.Equals(guid, StringComparison.OrdinalIgnoreCase))
+                    return true;
+
+            return false;
+        }
+
+        private static IEnumerable<Project> GetChildProjects(Project parent)
+        {
+            try
+            {
+                if (!parent.IsKind("{66A26720-8FB5-11D2-AA7E-00C04F688DDE}") && parent.Collection == null) // Unloaded
+                    return Enumerable.Empty<Project>();
+
+                if (!string.IsNullOrEmpty(parent.FullName))
+                    return new[] { parent };
+            }
+            catch (COMException)
+            {
+                return Enumerable.Empty<Project>();
+            }
+
+            return parent.ProjectItems
+                .Cast<ProjectItem>()
+                .Where(p => p.SubProject != null)
+                .SelectMany(p => GetChildProjects(p.SubProject));
+        }
+
+        public static Project GetActiveProject()
+        {
+            try
+            {
+                if (Dte.ActiveSolutionProjects is Array activeSolutionProjects && activeSolutionProjects.Length > 0)
+                    return activeSolutionProjects.GetValue(0) as Project;
+
+                var doc = Dte.ActiveDocument;
+
+                if (doc != null && !string.IsNullOrEmpty(doc.FullName))
+                {
+                    var item = Dte.Solution?.FindProjectItem(doc.FullName);
+
+                    if (item != null)
+                        return item.ContainingProject;
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputWindow.Log("Error getting the active project" + ex);
+            }
+
+            return null;
+        }
+
+        //public static IWpfTextView GetCurentTextView()
+        //{
+        //    IComponentModel componentModel = GetComponentModel();
+        //    if (componentModel == null) return null;
+        //    IVsEditorAdaptersFactoryService
+        //        editorAdapter = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+
+        //    return editorAdapter.GetWpfTextView(GetCurrentNativeTextView());
+        //}
+
+        public static IVsTextView GetCurrentNativeTextView()
+        {
+            var textManager = (IVsTextManager)ServiceProvider.GlobalProvider.GetService(typeof(SVsTextManager));
+
+            ErrorHandler.ThrowOnFailure(textManager.GetActiveView(1, null, out var activeView));
+            return activeView;
+        }
+
+        //public static IComponentModel GetComponentModel()
+        //{
+        //    return (IComponentModel)AddAnyFilePackage.GetGlobalService(typeof(SComponentModel));
+        //}
+
+        public static object GetSelectedItem()
+        {
+            object selectedObject = null;
+
+            var monitorSelection = (IVsMonitorSelection)Package.GetGlobalService(typeof(SVsShellMonitorSelection));
+
+            try
+            {
+                monitorSelection.GetCurrentSelection(
+                    out var hierarchyPointer,
+                    out var itemId,
+                    out var multiItemSelect,
+                    out var selectionContainerPointer);
+
+
+                if (Marshal.GetTypedObjectForIUnknown(
+                    hierarchyPointer,
+                    typeof(IVsHierarchy)) is IVsHierarchy selectedHierarchy)
+                    ErrorHandler.ThrowOnFailure(
+                        selectedHierarchy.GetProperty(
+                            itemId,
+                            (int)__VSHPROPID.VSHPROPID_ExtObject,
+                            out selectedObject));
+
+                Marshal.Release(hierarchyPointer);
+                Marshal.Release(selectionContainerPointer);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+
+            return selectedObject;
+        }
+    }
+
+    public static class ProjectTypes
+    {
+        public const string ASPNET_5 = "{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}";
+        public const string DOTNET_Core = "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}";
+        public const string WEBSITE_PROJECT = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+        public const string UNIVERSAL_APP = "{262852C6-CD72-467D-83FE-5EEB1973A190}";
+        public const string NODE_JS = "{9092AA53-FB77-4645-B42D-1CCCA6BD08BD}";
+        public const string SSDT = "{00d1a9c2-b5f0-4af3-8072-f6c62b433612}";
+    }
+}
