97e552ba2cbf06ce2eeecb4b730d67f26fc2476e
Christian Resma Helle <christian.helle@outlook.com>
2019-05-28 22:37:26 +0200

    Create extension methods to SwaggerDocument

---
+++ src/ApiClientCodeGen.VSIX/Extensions/SwaggerDocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using NSwag;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
+{
+    public static class SwaggerDocumentExtensions
+    {
+        public static string GenerateClassName(this SwaggerDocument document)
+            => string.IsNullOrWhiteSpace(document.Info?.Title)
+                ? "ApiClient"
+                : $"{GetSanitizeTitle(document)}Client";
+
+        public static string GetSanitizeTitle(this SwaggerDocument document)
+            => RemoveCharacters(
+                document.Info.Title,
+                "Swagger", " ", ".", "-");
+
+        private static string RemoveCharacters(string source, params string[] removeChars)
+            => removeChars.Aggregate(
+                source,
+                (current, c) => current.Replace(c, string.Empty));
+
+    }
+}
