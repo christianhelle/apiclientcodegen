885f168c3bc4cc6276a4717b0e13db6b86f70bb8
Christian Resma Helle <christian.helle@outlook.com>
2019-06-09 17:39:53 +0200

    Implement helper class for retrieving path to java.exe from the JAVA_HOME env variable

---
+++ src/ApiClientCodeGen.VSIX/Options/PathProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
+{
+    public class PathProvider
+    {
+        public string GetJavaPath()
+        {
+            var javaHome = Environment.GetEnvironmentVariable("JAVA_HOME");
+            var javaExe = Path.Combine(javaHome, "bin\\java.exe");
+            return javaExe;
+        }
+    }
+}
