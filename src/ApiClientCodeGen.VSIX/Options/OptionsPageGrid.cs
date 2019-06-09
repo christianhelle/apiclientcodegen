bedcda5af4f566f6ed8821ef5592739f297624d5
Christian Resma Helle <christian.helle@outlook.com>
2019-06-09 18:43:04 +0200

    Create option page for NSwag options

--- src/ApiClientCodeGen.VSIX/Options/OptionsPageGrid.cs
+++ src/ApiClientCodeGen.VSIX/Options/OptionsPageGrid.cs
@@ -30,5 +30,31 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
         public string NSwagPath { get; set; }
         #endregion
 
+        #region NSwag
+        [Category("NSwag")]
+        [DisplayName("Inject HttpClient")]
+        [Description("Set this to TRUE to generate the constructor that accepts HttpClient")]
+        public bool InjectHttpClient { get; set; } = true;
+
+        [Category("NSwag")]
+        [DisplayName("Generate Interfaces")]
+        [Description("Set this to TRUE to generate client interfaces")]
+        public bool GenerateClientInterfaces { get; set; } = true;
+
+        [Category("NSwag")]
+        [DisplayName("Generate DTO types")]
+        [Description("Set this to TRUE to generate DTO types")]
+        public bool GenerateDtoTypes { get; set; } = true;
+
+        [Category("NSwag")]
+        [DisplayName("Use Base URL")]
+        [Description("Set this to TRUE to include a base URL for every HTTP request")]
+        public bool UseBaseUrl { get; set; } = false;
+
+        [Category("NSwag")]
+        [DisplayName("C# Class Style")]
+        [Description("POCO (Plain Old C# Objects), Inpc (Implements INotifyPropertyChanged), Prism (Prism base class), Records (readonly POCO)")]
+        public CSharpClassStyle ClassStyle { get; set; } 
+        #endregion
     }
 }
