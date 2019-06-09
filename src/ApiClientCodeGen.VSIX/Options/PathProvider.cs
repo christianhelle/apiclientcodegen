d97720febf54d732805fec6d115e46e6cb2202c6
Christian Resma Helle <christian.helle@outlook.com>
2019-06-09 18:37:23 +0200

    Implement helper class for providing file paths to java.exe, npm.cmd, and nswag.exe

--- src/ApiClientCodeGen.VSIX/Options/PathProvider.cs
+++ src/ApiClientCodeGen.VSIX/Options/PathProvider.cs
@@ -3,13 +3,31 @@ using System.IO;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
 {
-    public class PathProvider
+    public static class PathProvider
     {
-        public string GetJavaPath()
+        public static string GetJavaPath()
         {
             var javaHome = Environment.GetEnvironmentVariable("JAVA_HOME");
             var javaExe = Path.Combine(javaHome, "bin\\java.exe");
             return javaExe;
         }
+
+        public static string GetNpmPath()
+        {
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            var programFiles64 = programFiles.Replace(" (x86)", newValue: string.Empty);
+
+            var npmCommand = Path.Combine(programFiles, "nodejs\\npm.cmd");
+            if (File.Exists(npmCommand))
+                return npmCommand;
+
+            npmCommand = Path.Combine(programFiles64, "nodejs\\npm.cmd");
+            return !File.Exists(npmCommand) ? null : npmCommand;
+        }
+
+        public static string GetNSwagPath()
+            => Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+                "Rico Suter\\NSwagStudio\\Win\\NSwag.exe");
     }
 }
