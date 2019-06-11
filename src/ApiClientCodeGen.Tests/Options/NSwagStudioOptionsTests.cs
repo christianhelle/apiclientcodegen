d19ef148ff0eb06b47121067ec8f5d9bd6c8d92e
Christian Helle <che@cloudeon.com>
2019-06-11 16:41:03 +0200

    Implement NSwagStudioOptions

---
+++ src/ApiClientCodeGen.Tests/Options/NSwagStudioOptionsTests.cs
@@ -0,0 +1,97 @@
+﻿using AutoFixture;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests.Options
+{
+    [TestClass]
+    public class NSwagStudioOptionsTests
+    {
+        private INSwagStudioOptions options;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var mock = new Mock<INSwagStudioOptions>();
+            options = mock.Object;
+        }
+
+        [TestMethod]
+        public void Extends_NSwagCSharpOptions() 
+            => typeof(NSwagStudioOptions)
+                .Should()
+                .BeAssignableTo<NSwagCSharpOptions>();
+
+        [TestMethod]
+        public void Reads_InjectHttpClient_From_Options()
+            => new NSwagStudioOptions(options)
+                .InjectHttpClient
+                .Should()
+                .Be(options.InjectHttpClient);
+
+        [TestMethod]
+        public void Reads_GenerateClientInterfaces_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateClientInterfaces
+                .Should()
+                .Be(options.GenerateClientInterfaces);
+
+        [TestMethod]
+        public void Reads_GenerateDtoTypes_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateDtoTypes
+                .Should()
+                .Be(options.GenerateDtoTypes);
+
+        [TestMethod]
+        public void Reads_UseBaseUrl_From_Options()
+            => new NSwagStudioOptions(options)
+                .UseBaseUrl
+                .Should()
+                .Be(options.UseBaseUrl);
+
+        [TestMethod]
+        public void Reads_ClassStyle_From_Options()
+            => new NSwagStudioOptions(options)
+                .ClassStyle
+                .Should()
+                .Be(options.ClassStyle);
+
+        [TestMethod]
+        public void Reads_GenerateResponseClasses_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateResponseClasses
+                .Should()
+                .Be(options.GenerateResponseClasses);
+
+        [TestMethod]
+        public void Reads_GenerateJsonMethods_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateJsonMethods
+                .Should()
+                .Be(options.GenerateJsonMethods);
+
+        [TestMethod]
+        public void Reads_RequiredPropertiesMustBeDefined_From_Options()
+            => new NSwagStudioOptions(options)
+                .RequiredPropertiesMustBeDefined
+                .Should()
+                .Be(options.RequiredPropertiesMustBeDefined);
+
+        [TestMethod]
+        public void Reads_GenerateDefaultValues_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateDefaultValues
+                .Should()
+                .Be(options.GenerateDefaultValues);
+
+        [TestMethod]
+        public void Reads_GenerateDataAnnotations_From_Options()
+            => new NSwagStudioOptions(options)
+                .GenerateDataAnnotations
+                .Should()
+                .Be(options.GenerateDataAnnotations);
+    }
+}
---
+++ src/ApiClientCodeGen.VSIX/Options/NSwagStudioOptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
+{
+    public class NSwagStudioOptions : NSwagCSharpOptions, INSwagStudioOptions
+    {
+        public NSwagStudioOptions(INSwagStudioOptions options = null)
+            : base(options)
+        {
+            try
+            {
+                if (options == null)
+                    options = (INSwagStudioOptions)VsPackage.Instance.GetDialogPage(typeof(NSwagStudioOptionsPage));
+
+                GenerateResponseClasses = options.GenerateResponseClasses;
+                GenerateJsonMethods = options.GenerateJsonMethods;
+                RequiredPropertiesMustBeDefined = options.RequiredPropertiesMustBeDefined;
+                GenerateDefaultValues = options.GenerateDefaultValues;
+                GenerateDataAnnotations = options.GenerateDataAnnotations;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e);
+                Trace.WriteLine(Environment.NewLine);
+                Trace.WriteLine("Error reading user options. Reverting to default values");
+                Trace.WriteLine("GenerateResponseClasses = true");
+                Trace.WriteLine("GenerateJsonMethods = true");
+                Trace.WriteLine("RequiredPropertiesMustBeDefined = true");
+                Trace.WriteLine("GenerateDefaultValues = true");
+                Trace.WriteLine("GenerateDataAnnotations = true");
+
+                GenerateResponseClasses = true;
+                GenerateJsonMethods = true;
+                RequiredPropertiesMustBeDefined = true;
+                GenerateDefaultValues = true;
+                GenerateDataAnnotations = true;
+            }
+        }
+
+        public bool GenerateResponseClasses { get; set; }
+        public bool GenerateJsonMethods { get; set; }
+        public bool RequiredPropertiesMustBeDefined { get; set; }
+        public bool GenerateDefaultValues { get; set; }
+        public bool GenerateDataAnnotations { get; set; }
+    }
+}
