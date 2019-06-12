c999b10c0aa6697c7165dd81391de4510006cfb6
Christian Resma Helle <christian.helle@outlook.com>
2019-06-12 23:39:17 +0200

    Move all NSwag Studio options to be under the same category

--- src/ApiClientCodeGen.VSIX/Options/NSwagStudioOptionsPage.cs
+++ src/ApiClientCodeGen.VSIX/Options/NSwagStudioOptionsPage.cs
@@ -1,10 +1,37 @@
 ﻿using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+using NJsonSchema.CodeGeneration.CSharp;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
 {
-    public class NSwagStudioOptionsPage : NSwagOptionsPage, INSwagStudioOptions
+    public class NSwagStudioOptionsPage : DialogPage, INSwagStudioOptions
     {
-        public new const string Name = "NSwag Studio";
+        public const string Name = "NSwag Studio";
+
+        [Category(Name)]
+        [DisplayName("Inject HttpClient")]
+        [Description("Set this to TRUE to generate the constructor that accepts HttpClient")]
+        public bool InjectHttpClient { get; set; } = true;
+
+        [Category(Name)]
+        [DisplayName("Generate Interfaces")]
+        [Description("Set this to TRUE to generate client interfaces")]
+        public bool GenerateClientInterfaces { get; set; } = true;
+
+        [Category(Name)]
+        [DisplayName("Generate DTO types")]
+        [Description("Set this to TRUE to generate DTO types")]
+        public bool GenerateDtoTypes { get; set; } = true;
+
+        [Category(Name)]
+        [DisplayName("Use Base URL")]
+        [Description("Set this to TRUE to include a base URL for every HTTP request")]
+        public bool UseBaseUrl { get; set; } = false;
+
+        [Category(Name)]
+        [DisplayName("C# Class Style")]
+        [Description("POCO (Plain Old C# Objects), Inpc (Implements INotifyPropertyChanged), Prism (Prism base class), Records (readonly POCO)")]
+        public CSharpClassStyle ClassStyle { get; set; }
         
         [Category(Name)]
         [DisplayName("Generate Response Classes")]
