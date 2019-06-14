37ca88213caf211b2686825920607716a8d97844
Christian Resma Helle <christian.helle@outlook.com>
2019-06-14 21:32:34 +0200

    Add constraint to TDialogPage to ensure that only DialogPage types are used

--- src/ApiClientCodeGen.VSIX/Options/OptionsFactory.cs
+++ src/ApiClientCodeGen.VSIX/Options/OptionsFactory.cs
@@ -1,9 +1,12 @@
-﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options
 {
     public class OptionsFactory : IOptionsFactory
     {
-        public TOptions Create<TOptions, TDialogPage>() 
-            where TOptions : class 
+        public TOptions Create<TOptions, TDialogPage>()
+            where TOptions : class
+            where TDialogPage : DialogPage
             => VsPackage.Instance.GetDialogPage(typeof(TDialogPage)) as TOptions;
     }
 }
