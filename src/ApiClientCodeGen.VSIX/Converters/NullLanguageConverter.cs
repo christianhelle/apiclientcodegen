380ec3fcd3013668e72a07e99c3cf1b00971a062
Christian Resma Helle <christian.helle@outlook.com>
2019-06-27 23:41:04 +0200

    Create ILanguageConverter null pattern implementation

---
+++ src/ApiClientCodeGen.VSIX/Converters/NullLanguageConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Converters
+{
+    public class NullLanguageConverter : ILanguageConverter
+    {
+        public Task<string> Convert(string code) => Task.FromResult(code);
+    }
+}
