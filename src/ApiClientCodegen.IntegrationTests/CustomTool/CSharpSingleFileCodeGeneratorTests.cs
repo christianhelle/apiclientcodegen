d6a9bd0f26fee3a4e55a4db493c8e9004b31daef
Christian Resma Helle <christian.helle@outlook.com>
2019-06-28 00:29:06 +0200

    Update C# SingleFileCodeGenerator integration tests

---
+++ src/ApiClientCodegen.IntegrationTests/CustomTool/CSharpSingleFileCodeGeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using FluentAssertions;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.CustomTool
+{
+    [TestClass]
+    [TestCategory("SkipWhenLiveUnitTesting")]
+    [DeploymentItem("Resources/Swagger.json")]
+    public class CSharpSingleFileCodeGeneratorTests
+    {
+        private const SupportedLanguage Language = SupportedLanguage.CSharp;
+
+        [TestMethod]
+        public void AutoRest_VisualBasic_Test() => Assert(SupportedCodeGenerator.AutoRest);
+        
+        [TestMethod]
+        public void NSwag_VisualBasic_Test() => Assert(SupportedCodeGenerator.AutoRest);
+        
+        [TestMethod]
+        public void Swagger_VisualBasic_Test() => Assert(SupportedCodeGenerator.AutoRest);
+        
+        [TestMethod]
+        public void OpenApi_VisualBasic_Test() => Assert(SupportedCodeGenerator.AutoRest);
+
+        private static void Assert(SupportedCodeGenerator generator)
+        {
+            var rgbOutputFileContents = new[] {IntPtr.Zero};
+            var progressMock = new Mock<IVsGeneratorProgress>();
+
+            var sut = new VisualBasicSingleFileCodeGenerator(generator);
+
+            var result = sut.Generate(
+                Path.GetFullPath("Swagger.json"),
+                string.Empty,
+                typeof(VisualBasicSingleFileCodeGeneratorTests).Namespace,
+                rgbOutputFileContents,
+                out var pcbOutput,
+                progressMock.Object);
+
+            result.Should().Be(0);
+            pcbOutput.Should().NotBe(0);
+            rgbOutputFileContents[0].Should().NotBe(IntPtr.Zero);
+        }
+    }
+}
