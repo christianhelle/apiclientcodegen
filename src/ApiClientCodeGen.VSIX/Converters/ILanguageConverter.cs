f8ae1581f05b9b8d5f8d61f1d9cfb3f4cb8f1c0d
Christian Resma Helle <christian.helle@outlook.com>
2019-06-27 23:40:50 +0200

    Introduce ILanguageConverter

---
+++ src/ApiClientCodeGen.VSIX/Converters/ILanguageConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Converters
+{
+    public interface ILanguageConverter
+    {
+        Task<string> Convert(string code);
+    }
+}
