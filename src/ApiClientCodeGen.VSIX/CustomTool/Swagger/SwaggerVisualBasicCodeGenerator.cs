10d0c9b4abe5a835a7295b14771b0bab6f2749b6
Christian Resma Helle <christian.helle@outlook.com>
2019-07-01 22:50:28 +0200

    Create Visual Basic custom tools

---
+++ src/ApiClientCodeGen.VSIX/CustomTool/Swagger/SwaggerVisualBasicCodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.Swagger
+{
+    [Guid("CE6638E5-E6E3-4EF5-90D7-E80DBA61C933")]
+    [ComVisible(true)]
+    [ProvideObject(typeof(SwaggerVisualBasicCodeGenerator))]
+    [CodeGeneratorRegistration(typeof(SwaggerVisualBasicCodeGenerator),
+                              Description,
+                              ProvideCodeGeneratorAttribute.VisualBasicProjectGuid,
+                              GeneratesDesignTimeSource = true,
+                              GeneratorRegKeyName = "SwaggerCodeGenerator")]
+    public class SwaggerVisualBasicCodeGenerator : SwaggerCodeGenerator
+    {
+        public const string Description = "VB.NET Swagger API Client Code Generator";
+
+        public SwaggerVisualBasicCodeGenerator() 
+            : base(SupportedLanguage.VisualBasic)
+        {
+        }
+
+        public override int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".vb";
+            return 0;
+        }
+    }
+}
