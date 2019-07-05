a4f3212131f7847812803c0386a21398d1dd1977
Christian Resma Helle <christian.helle@outlook.com>
2019-07-05 15:39:18 +0200

    Add CSharpSingleFIleCodeGenerator to integration tests project

---
+++ src/ApiClientCodegen.IntegrationTests/CustomTool/CSharpSingleFileCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Utility;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.CustomTool
+{
+    internal class CSharpSingleFileCodeGenerator : SingleFileCodeGenerator
+    {
+        internal CSharpSingleFileCodeGenerator(
+            SupportedCodeGenerator supportedCodeGenerator)
+            : base(
+                supportedCodeGenerator, 
+                SupportedLanguage.CSharp)
+        {
+        }
+
+        public override int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".cs";
+            return 0;
+        }
+    }
+}
