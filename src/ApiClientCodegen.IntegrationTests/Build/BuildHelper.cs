d7a22f7574e26619850dd7e078266bea3e9a57f9
Christian Resma Helle <christian.helle@outlook.com>
2019-07-06 10:47:41 +0200

    Introduce helper class for building .NET projects from code

---
+++ src/ApiClientCodegen.IntegrationTests/Build/BuildHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Build
+{
+    public sealed class BuildHelper
+    {
+        public static void BuildCSharp(ProjectTypes projecType, string generatedCode)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString("N"));
+            Directory.CreateDirectory(path);
+            var projectFile = Path.Combine(path, "Project.csproj");
+            var projectContents = GetProjectContents(projecType);
+            Trace.WriteLine(projectContents);
+            File.WriteAllText(projectFile, projectContents);
+            File.WriteAllText(Path.Combine(path, "Generated.cs"), generatedCode);
+            ProcessHelper.StartProcess("dotnet.exe", $"build \"{projectFile}\"");
+        }
+
+        private static string GetProjectContents(ProjectTypes projecType)
+        {
+            switch (projecType)
+            {
+                case ProjectTypes.DotNetCoreApp:
+                    return ProjectFileContents.NetCoreApp;
+                
+                case ProjectTypes.DotNetStandardLibrary:
+                    return ProjectFileContents.NetStandardLibrary;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(projecType), projecType, null);
+            }
+        }
+    }
+}
