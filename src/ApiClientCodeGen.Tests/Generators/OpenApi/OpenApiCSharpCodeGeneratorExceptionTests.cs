e69210dd3c134f0d5bece32622dd3a10fcd73ebe
Christian Resma Helle <christian.helle@outlook.com>
2019-07-12 22:56:31 +0200

    Add constructor tests to OpenApi and Swagger CSharpCodeGenerators

---
+++ src/ApiClientCodeGen.Tests/Generators/OpenApi/OpenApiCSharpCodeGeneratorExceptionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.OpenApi;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests.Generators.OpenApi
+{
+    [TestClass]
+    public class OpenApiCSharpCodeGeneratorExceptionTests
+    {
+        [TestMethod]
+        public void Constructor_Requires_SwaggerFile()
+            => new Action(() => new OpenApiCSharpCodeGenerator(null, null, null))
+                .Should()
+                .ThrowExactly<ArgumentNullException>();
+        
+        [TestMethod]
+        public void Constructor_Requires_DefaultNamespace()
+            => new Action(() => new OpenApiCSharpCodeGenerator("", null, null))
+                .Should()
+                .ThrowExactly<ArgumentNullException>();
+        
+        [TestMethod]
+        public void Constructor_Requires_Options()
+            => new Action(() => new OpenApiCSharpCodeGenerator("", "", null))
+                .Should()
+                .ThrowExactly<ArgumentNullException>();
+    }
+}
