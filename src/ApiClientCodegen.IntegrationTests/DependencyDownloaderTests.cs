7fac6f624715b0ac98291134d6e976d15a004383
Christian Resma Helle <christian.helle@outlook.com>
2019-09-20 14:09:44 +0200

    Implement DependencyDownloader integration tests

---
+++ src/ApiClientCodegen.IntegrationTests/DependencyDownloaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests
+{
+    [TestClass]
+    public class DependencyDownloaderTests
+    {
+        [TestMethod]
+        public void InstallOpenApiGenerator_Returns_Path()
+            => DependencyDownloader
+                .InstallOpenApiGenerator()
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [TestMethod]
+        public void InstallSwaggerCodegenCli_Returns_Path()
+            => DependencyDownloader
+                .InstallSwaggerCodegenCli()
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [TestMethod]
+        public void InstallAutoRest_Returns_Path()
+            => new Action(DependencyDownloader.InstallAutoRest)
+                .Should()
+                .NotThrow();
+    }
+}
