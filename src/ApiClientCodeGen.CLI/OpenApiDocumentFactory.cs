949fd8da4c66e4990ea8e836c2dfa67694d306b8
Christian Resma Helle <christian.helle@outlook.com>
2019-11-19 22:54:19 +0100

    Create IOpenApiDocumentFactory default implementation

---
+++ src/ApiClientCodeGen.CLI/OpenApiDocumentFactory.cs
@@ -0,0 +1,15 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
+using NSwag;
+
+namespace ApiClientCodeGen.CLI
+{
+    public class OpenApiDocumentFactory : IOpenApiDocumentFactory
+    {
+        public OpenApiDocument GetDocument(string swaggerFile)
+        {
+            return OpenApiDocument.FromFileAsync(swaggerFile)
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
