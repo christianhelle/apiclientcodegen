004cd9640b8e2b0a0883a85149674ba6ee61a8de
Christian Resma Helle <christian.helle@outlook.com>
2019-11-19 22:53:58 +0100

    Create default INSwagOptions implementation

---
+++ src/ApiClientCodeGen.CLI/NSwagOptions.cs
@@ -0,0 +1,15 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace ApiClientCodeGen.CLI
+{
+    public class NSwagOptions : INSwagOptions
+    {
+        public bool InjectHttpClient { get; } = true;
+        public bool GenerateClientInterfaces { get; } = true;
+        public bool GenerateDtoTypes { get; } = true;
+        public CSharpClassStyle ClassStyle { get; } = CSharpClassStyle.Poco;
+        public bool UseDocumentTitle { get; }
+        public bool UseBaseUrl { get; }
+    }
+}
