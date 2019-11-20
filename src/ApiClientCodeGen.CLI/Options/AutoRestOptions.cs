5c41e589cf58aac80317415e1bb438f4bbe81be5
Christian Resma Helle <christian.helle@outlook.com>
2019-11-20 21:00:14 +0100

    Implement IAutoRestOptions for CLI app

---
+++ src/ApiClientCodeGen.CLI/Options/AutoRestOptions.cs
@@ -0,0 +1,14 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
+
+namespace ApiClientCodeGen.CLI.Options
+{
+    public class AutoRestOptions : IAutoRestOptions
+    {
+        public bool AddCredentials { get; set; }
+        public bool OverrideClientName { get; set; }
+        public bool UseInternalConstructors { get; set; }
+        public SyncMethodOptions SyncMethods { get; set; }
+        public bool UseDateTimeOffset { get; set; }
+        public bool ClientSideValidation { get; set; }
+    }
+}
