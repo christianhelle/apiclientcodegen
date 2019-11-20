3e0810e146f08797a4ab62667571bce150c7b4c7
Christian Resma Helle <christian.helle@outlook.com>
2019-11-20 21:01:10 +0100

    Move NSwagOptions to Options folder and update namespaces

--- src/ApiClientCodeGen.CLI/Options/NSwagOptions.cs
+++ src/ApiClientCodeGen.CLI/Options/NSwagOptions.cs
@@ -1,7 +1,7 @@
 ﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
 using NJsonSchema.CodeGeneration.CSharp;
 
-namespace ApiClientCodeGen.CLI
+namespace ApiClientCodeGen.CLI.Options
 {
     public class NSwagOptions : INSwagOptions
     {
