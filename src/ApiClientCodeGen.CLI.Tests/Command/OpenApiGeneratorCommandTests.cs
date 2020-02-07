33c466a2be2ed917ecfb3d1327d59a6025b90906
Christian Helle <crh@delegate.dk>
2020-02-07 16:44:19 +0100

    Update OpenApiGeneratorCommand to use the factory

---
+++ src/ApiClientCodeGen.CLI.Tests/Command/OpenApiGeneratorCommandTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using ApiClientCodeGen.CLI.Commands;
+using ApiClientCodeGen.CLI.Tests.Infrastructure;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ApiClientCodeGen.CLI.Tests.Command
+{
+    public class OpenApiGeneratorCommandTests
+    {   
+        [Theory, AutoMoqData]
+        public void DefaultNamespace_Should_NotBeNullOrWhiteSpace(OpenApiGeneratorCommand sut)
+            => sut.DefaultNamespace.Should().NotBeNullOrWhiteSpace();
+
+        [Theory, AutoMoqData]
+        public void SwaggerFile_Should_NotBeNullOrWhiteSpace(OpenApiGeneratorCommand sut)
+            => sut.SwaggerFile.Should().NotBeNullOrWhiteSpace();
+
+        [Theory, AutoMoqData]
+        public void OutputFile_Should_NotBeNullOrWhiteSpace(OpenApiGeneratorCommand sut)
+            => sut.OutputFile.Should().NotBeNullOrWhiteSpace();
+
+        [Theory, AutoMoqData]
+        public void CreateGenerator_Should_NotNull(OpenApiGeneratorCommand sut)
+            => sut.CreateGenerator().Should().NotBeNull();
+
+        [Theory, AutoMoqData]
+        public void OnExecuteAsync_Should_NotThrow(
+            IConsoleOutput console,
+            IProgressReporter progressReporter,
+            IProcessLauncher processLauncher,
+            IGeneralOptions options,
+            IOpenApiGeneratorFactory codeGeneratorFactory,
+            ICodeGenerator generator,
+            string code)
+        {
+            var sut = new OpenApiGeneratorCommand(
+                console,
+                progressReporter, 
+                options,
+                processLauncher, 
+                codeGeneratorFactory);
+            
+            Mock.Get(generator)
+                .Setup(c => c.GenerateCode(progressReporter))
+                .Returns(code);
+            
+            new Func<Task>(sut.OnExecuteAsync).Should().NotThrow();
+        }
+    }
+}
--- src/ApiClientCodeGen.CLI/Commands/OpenApiGeneratorCommand.cs
+++ src/ApiClientCodeGen.CLI/Commands/OpenApiGeneratorCommand.cs
@@ -12,19 +12,22 @@ namespace ApiClientCodeGen.CLI.Commands
     {
         private readonly IGeneralOptions options;
         private readonly IProcessLauncher processLauncher;
+        private readonly IOpenApiGeneratorFactory generatorFactory;
 
         public OpenApiGeneratorCommand(
             IConsoleOutput console,
             IProgressReporter progressReporter,
             IGeneralOptions options,
-            IProcessLauncher processLauncher) : base(console, progressReporter)
+            IProcessLauncher processLauncher,
+            IOpenApiGeneratorFactory generatorFactory) : base(console, progressReporter)
         {
             this.options = options ?? throw new ArgumentNullException(nameof(options));
             this.processLauncher = processLauncher ?? throw new ArgumentNullException(nameof(processLauncher));
+            this.generatorFactory = generatorFactory ?? throw new ArgumentNullException(nameof(generatorFactory));
         }
 
         public override ICodeGenerator CreateGenerator()
-            => new OpenApiCSharpCodeGenerator(
+            => generatorFactory.Create(
                 SwaggerFile,
                 DefaultNamespace,
                 options,
