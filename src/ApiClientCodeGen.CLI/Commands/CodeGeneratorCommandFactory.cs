f568d6cc24223cb936af5d44055370bf3315b47b
Christian Resma Helle <christian.helle@outlook.com>
2020-02-07 15:49:24 +0100

    Introduce ICodeGeneratorCommandFactory

---
+++ src/ApiClientCodeGen.CLI/Commands/CodeGeneratorCommandFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.AutoRest;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
+
+namespace ApiClientCodeGen.CLI.Commands
+{
+    public interface ICodeGeneratorCommandFactory
+    {
+        ICodeGenerator Create<T>(
+            string swaggerFile, 
+            string defaultNamespace, 
+            object options, 
+            IProcessLauncher processLauncher)
+            where T : CodeGeneratorCommand;
+    }
+
+    public class CodeGeneratorCommandFactory : ICodeGeneratorCommandFactory
+    {
+        public ICodeGenerator Create<T>(
+            string swaggerFile, 
+            string defaultNamespace, 
+            object options, 
+            IProcessLauncher processLauncher)
+            where T : CodeGeneratorCommand
+        {
+            if (typeof(T) == typeof(AutoRestCommand))
+                return new AutoRestCSharpCodeGenerator(
+                    swaggerFile,
+                    defaultNamespace,
+                    (IAutoRestOptions) options,
+                    processLauncher);
+
+            throw new NotSupportedException();
+        }
+    }
+}
