be1469c2ef1bc777d3f610eb49c94cf9a0c4e527
Christian Helle <crh@delegate.dk>
2020-03-03 13:10:42 +0100

    Introduce ConsoleOutputTraceListener

---
+++ src/ApiClientCodeGen.CLI/Logging/ConsoleOutputTraceListener.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ApiClientCodeGen.CLI
+{
+    [ExcludeFromCodeCoverage]
+    public class ConsoleOutputTraceListener : TraceListener
+    {
+        private readonly IConsoleOutput console;
+
+        public ConsoleOutputTraceListener(IConsoleOutput console)
+        {
+            this.console = console;
+        }
+        
+        public override void Write(string message)
+            => console.WriteLine(message);
+
+        public override void WriteLine(string message) 
+            => console.WriteLine(message);
+    }
+}
