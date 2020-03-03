8d0e944d5f52a29df217822bd78b04640c432728
Christian Helle <crh@delegate.dk>
2020-03-03 13:33:33 +0100

    Enable verbose output on test.ps1 script

--- src/ApiClientCodeGen.CLI/test.ps1
+++ src/ApiClientCodeGen.CLI/test.ps1
@@ -1,13 +1,13 @@
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-dotnet run -- autorest ../Swagger.json GeneratedCode ../AutoRestOutput.cs
+dotnet run -- --verbose autorest ../Swagger.json GeneratedCode ../AutoRestOutput.cs
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run -- nswag ../Swagger.json GeneratedCode ../NSwagOutput.cs
+dotnet run -- --verbose nswag ../Swagger.json GeneratedCode ../NSwagOutput.cs
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run -- swagger ../Swagger.json GeneratedCode ../SwaggerOutput.cs
+dotnet run -- --verbose swagger ../Swagger.json GeneratedCode ../SwaggerOutput.cs
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run -- openapi ../Swagger.json GeneratedCode ../OpenApiOutput.cs
+dotnet run -- --verbose openapi ../Swagger.json GeneratedCode ../OpenApiOutput.cs
 
 Write-Host "`r`n"
