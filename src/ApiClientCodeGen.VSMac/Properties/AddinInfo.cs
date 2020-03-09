2bc04483ef395fbe00b1f6a767be5533d4384788
Christian Helle <crh@delegate.dk>
2020-03-09 11:10:34 +0100

    Create empty Visual Studio for Mac extension

---
+++ src/ApiClientCodeGen.VSMac/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "ApiClientCodeGen.VSMac",
+    Namespace = "ApiClientCodeGen.VSMac",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("ApiClientCodeGen")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("A collection of code generators for Swagger / OpenAPI specification files")]
+[assembly: AddinAuthor("Christian Resma Helle")]
