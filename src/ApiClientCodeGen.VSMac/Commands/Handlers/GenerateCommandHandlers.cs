be1b64905b1fb0065c5ddfa11fc246f9ab76a06f
Christian Helle <crh@delegate.dk>
2020-03-10 17:13:43 +0100

    Implement GenerateNSwagStudioCommandHandler

--- src/ApiClientCodeGen.VSMac/Commands/Handlers/GenerateCommandHandlers.cs
+++ src/ApiClientCodeGen.VSMac/Commands/Handlers/GenerateCommandHandlers.cs
@@ -1,6 +1,13 @@
-﻿using MonoDevelop.Components.Commands;
+﻿using System.IO;
+using Cairo;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwagStudio;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
 using MonoDevelop.Ide;
 using MonoDevelop.Projects;
+using Path = System.IO.Path;
 
 namespace ApiClientCodeGen.VSMac.Commands.Handlers
 {
@@ -20,7 +27,7 @@ namespace ApiClientCodeGen.VSMac.Commands.Handlers
     {
     }
 
-    public abstract class GenerateCommandHandler : CommandHandler
+    public abstract class GenerateCommandHandler : BaseCommandHandler
     {
         protected override void Run()
         {
@@ -35,17 +42,34 @@ namespace ApiClientCodeGen.VSMac.Commands.Handlers
             var item = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
             info.Visible = item?.Name?.EndsWith(".json", System.StringComparison.OrdinalIgnoreCase) == true;
         }
-
-        protected void Hide(CommandInfo info)
-            => info.Visible = false;
     }
 
     public class GenerateNSwagStudioCommandHandler : GenerateCommandHandler
     {
+        private string nswagStudioFile;
+
+        protected override void Run() => GenerateCode();
+
+        protected override void Run(object dataItem) => GenerateCode();
+
         protected override void Update(CommandInfo info)
         {
             var item = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
             info.Visible = item?.Name?.EndsWith(".nswag", System.StringComparison.OrdinalIgnoreCase) == true;
+            nswagStudioFile = item?.FilePath;
+        }
+
+        private void GenerateCode()
+        {
+            if (string.IsNullOrWhiteSpace(nswagStudioFile))
+                return;
+
+            var codeGenerator = new NSwagStudioCodeGenerator(
+                nswagStudioFile,
+                new DefaultGeneralOptions(),
+                new ProcessLauncher(Path.GetDirectoryName(nswagStudioFile)));
+            
+            codeGenerator.GenerateCode(null);
         }
     }
 }
