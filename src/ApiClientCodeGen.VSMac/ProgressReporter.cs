d0e65a31379d2596c6b5f67feb2e930ce4b7bb41
Christian Helle <crh@delegate.dk>
2020-03-15 00:01:04 +0100

    Exclude ProgressReporter from code coverage

--- src/ApiClientCodeGen.VSMac/ProgressReporter.cs
+++ src/ApiClientCodeGen.VSMac/ProgressReporter.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
 using MonoDevelop.Core;
 
 namespace ApiClientCodeGen.VSMac
 {
+    [ExcludeFromCodeCoverage]
     public class ProgressReporter : IProgressReporter
     {
         private readonly ProgressMonitor monitor;
