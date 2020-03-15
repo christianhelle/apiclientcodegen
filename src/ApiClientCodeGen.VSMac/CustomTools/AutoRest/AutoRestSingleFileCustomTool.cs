2cc0981956094673dd14ed9bebea3066cda575eb
Christian Helle <crh@delegate.dk>
2020-03-15 10:00:26 +0100

    Implement AutoRestSingleFileCustomTool

--- src/ApiClientCodeGen.VSMac/CustomTools/AutoRest/AutoRestSingleFileCustomTool.cs
+++ src/ApiClientCodeGen.VSMac/CustomTools/AutoRest/AutoRestSingleFileCustomTool.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
 using MonoDevelop.Core;
 using MonoDevelop.Ide.CustomTools;
 using MonoDevelop.Projects;
@@ -9,12 +13,35 @@ namespace ApiClientCodeGen.VSMac.CustomTools.AutoRest
     {
         public const string GeneratorName = "AutoRestCodeGenerator";
 
-        protected override Task OnGenerate(
-            ProgressMonitor monitor, 
-            ProjectFile file, 
-            SingleFileCustomToolResult result)
+        private readonly IAutoRestCodeGeneratorFactory factory;
+        private readonly IAutoRestOptions options;
+        private readonly IProcessLauncher processLauncher;
+
+        public AutoRestSingleFileCustomTool()
+            : this(
+                Container.Instance.Resolve<IAutoRestCodeGeneratorFactory>(),
+                Container.Instance.Resolve<IAutoRestOptions>(),
+                Container.Instance.Resolve<IProcessLauncher>())
         {
-            throw new System.NotImplementedException();
         }
+
+        public AutoRestSingleFileCustomTool(
+            IAutoRestCodeGeneratorFactory factory,
+            IAutoRestOptions options,
+            IProcessLauncher processLauncher)
+        {
+            this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            this.options = options ?? throw new ArgumentNullException(nameof(options));
+            this.processLauncher = processLauncher ?? throw new ArgumentNullException(nameof(processLauncher));
+        }
+
+        protected override ICodeGenerator GetCodeGenerator(
+            string swaggerFile,
+            string customToolNamespace)
+            => factory.Create(
+                swaggerFile,
+                customToolNamespace,
+                options,
+                processLauncher);
     }
 }
