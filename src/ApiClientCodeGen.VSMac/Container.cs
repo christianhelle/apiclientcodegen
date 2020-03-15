988bc55ba4bcb6ad657b81e8ce3f17e1721c527c
Christian Helle <crh@delegate.dk>
2020-03-15 10:01:05 +0100

    Register dependencies used by AutoRestSingleFileCustomTool

--- src/ApiClientCodeGen.VSMac/Container.cs
+++ src/ApiClientCodeGen.VSMac/Container.cs
@@ -2,10 +2,12 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using ApiClientCodeGen.VSMac.Commands.NSwagStudio;
 using ApiClientCodeGen.VSMac.CustomTools;
+using ApiClientCodeGen.VSMac.CustomTools.AutoRest;
 using ApiClientCodeGen.VSMac.CustomTools.NSwag;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwagStudio;
@@ -43,6 +45,9 @@ namespace ApiClientCodeGen.VSMac
             services.AddSingleton<IOpenApiDocumentFactory, OpenApiDocumentFactory>();
             services.AddSingleton<INSwagCodeGeneratorFactory, NSwagCodeGeneratorFactory>();
             services.AddSingleton<NSwagCSharpCodeGenerator>();
+
+            services.AddSingleton<IAutoRestCodeGeneratorFactory, AutoRestCodeGeneratorFactory>();
+            services.AddSingleton<IAutoRestOptions, DefaultAutoRestOptions>();
 
             serviceProvider = services.BuildServiceProvider();
         }
