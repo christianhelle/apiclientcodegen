6ab6c643af8581af7c284b1c6f0732437718313e
Christian Resma Helle <christian.helle@outlook.com>
2020-03-26 21:20:36 +0100

    Introduce EmbeddedResources helper

---
+++ src/ApiClientCodeGen.Tests/Resources/EmbeddedResources.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests.Resources
+{
+    public static class EmbeddedResources
+    {
+        private static readonly Type Type = typeof(EmbeddedResources);
+        public static Stream GetStream(string name)
+            => Type.Assembly.GetManifestResourceStream(Type, name);
+    }
+}
---
+++ src/ApiClientCodeGen.Tests/TestWithResources.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests.Resources;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class TestWithResources
+    {
+        private static readonly object SyncLock = new object();
+
+        protected TestWithResources()
+        {
+            lock (SyncLock)
+            {
+                CreateFileFromEmbeddedResource("Swagger.json");
+                CreateFileFromEmbeddedResource("Swagger.nswag");
+            }
+        }
+
+        private static void CreateFileFromEmbeddedResource(string resourceName)
+        {
+            var directory = Directory.GetCurrentDirectory();
+            using var source = EmbeddedResources.GetStream(resourceName);
+            using var writer = File.Create(Path.Combine(directory, resourceName));
+            source.CopyTo(writer);
+        }
+
+        protected string ReadAllText(string resourceName)
+        {
+            using var stream = EmbeddedResources.GetStream(resourceName);
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
