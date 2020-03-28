ae81f1e9f564ce3451d7a20e4fb1558c52c0d97d
Christian Resma Helle <christian.helle@outlook.com>
2020-03-28 16:14:48 +0100

    Make DependencyDownloader extend TestWithResources

--- src/ApiClientCodeGen.Tests/DependencyDownloaderTests.cs
+++ src/ApiClientCodeGen.Tests/DependencyDownloaderTests.cs
@@ -2,62 +2,61 @@
 using System.IO;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
 using FluentAssertions;
-
+using Xunit;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Tests
 {
-    
-    public class DependencyDownloaderTests
+    public class DependencyDownloaderTests : TestWithResources
     {
-        [Xunit.Fact]
+        [Fact]
         public void InstallOpenApiGenerator_Returns_Path()
             => DependencyDownloader
                 .InstallOpenApiGenerator()
                 .Should()
                 .NotBeNullOrWhiteSpace();
 
-        [Xunit.Fact]
+        [Fact]
         public void InstallSwaggerCodegenCli_Returns_Path()
             => DependencyDownloader
                 .InstallSwaggerCodegenCli()
                 .Should()
                 .NotBeNullOrWhiteSpace();
                 
-        [Xunit.Fact]
+        [Fact]
         public void InstallOpenApiGenerator_With_Path_Returns_Path()
             => DependencyDownloader
                 .InstallOpenApiGenerator(Path.GetTempPath())
                 .Should()
                 .NotBeNullOrWhiteSpace();
 
-        [Xunit.Fact]
+        [Fact]
         public void InstallSwaggerCodegenCli_With_Path_Returns_Path()
             => DependencyDownloader
                 .InstallSwaggerCodegenCli(Path.GetTempPath())
                 .Should()
                 .NotBeNullOrWhiteSpace();
         
-        [Xunit.Fact]
+        [Fact]
         public void InstallOpenApiGenerator_Force_Returns_Path()
             => DependencyDownloader
                 .InstallOpenApiGenerator(forceDownload: true)
                 .Should()
                 .NotBeNullOrWhiteSpace();
 
-        [Xunit.Fact]
+        [Fact]
         public void InstallSwaggerCodegenCli_Force_Returns_Path()
             => DependencyDownloader
                 .InstallSwaggerCodegenCli(forceDownload: true)
                 .Should()
                 .NotBeNullOrWhiteSpace();
 
-        [Xunit.Fact]
+        [Fact]
         public void InstallAutoRest_Returns_Path()
             => new Action(DependencyDownloader.InstallAutoRest)
                 .Should()
                 .NotThrow();
 
-        [Xunit.Fact]
+        [Fact]
         public void InstallNSwag_Returns_Path()
             => new Action(DependencyDownloader.InstallNSwag)
                 .Should()
