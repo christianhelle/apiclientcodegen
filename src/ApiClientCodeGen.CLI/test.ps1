650065187abd45773c80cd4258a0777a02616d1d
Christian Helle <christian.helle@outlook.com>
2020-05-12 08:20:12 +0200

    Remove verbose output on CLI smoke test script

--- src/ApiClientCodeGen.CLI/test.ps1
+++ src/ApiClientCodeGen.CLI/test.ps1
@@ -1,13 +1,13 @@
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-dotnet run -- --verbose autorest ../Swagger.json GeneratedCode ../AutoRestOutput.cs
+dotnet run -- autorest ../Swagger.json GeneratedCode ../AutoRestOutput.cs
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run -- --verbose nswag ../Swagger.json GeneratedCode ../NSwagOutput.cs
+dotnet run -- nswag ../Swagger.json GeneratedCode ../NSwagOutput.cs
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run -- --verbose swagger ../Swagger.json GeneratedCode ../SwaggerOutput.cs
+dotnet run -- swagger ../Swagger.json GeneratedCode ../SwaggerOutput.cs
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run -- --verbose openapi ../Swagger.json GeneratedCode ../OpenApiOutput.cs
+dotnet run -- openapi ../Swagger.json GeneratedCode ../OpenApiOutput.cs
 
 Write-Host "`r`n"
