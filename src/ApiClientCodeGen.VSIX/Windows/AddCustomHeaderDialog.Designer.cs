fa736a257fe125cfabde616ccc9a8c230eb576ff
Christian Helle <christian.helle@outlook.com>
2020-05-12 15:10:00 +0200

    Introduce AddCustomHeaderDialog

---
+++ src/ApiClientCodeGen.VSIX/Windows/AddCustomHeaderDialog.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Windows
+{
+    partial class AddCustomHeaderDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbKey
+            // 
+            this.tbKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbKey.Location = new System.Drawing.Point(52, 15);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(488, 20);
+            this.tbKey.TabIndex = 0;
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(12, 18);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(25, 13);
+            this.lblKey.TabIndex = 1;
+            this.lblKey.Text = "Key";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(384, 74);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(465, 74);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(12, 44);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(34, 13);
+            this.lblValue.TabIndex = 7;
+            this.lblValue.Text = "Value";
+            // 
+            // tbValue
+            // 
+            this.tbValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbValue.Location = new System.Drawing.Point(52, 41);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(488, 20);
+            this.tbValue.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 89);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 8;
+            // 
+            // AddCustomHeaderDialog
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(552, 111);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.tbValue);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.tbKey);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddCustomHeaderDialog";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Custom HTTP Header";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.AddCustomHeaderDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ src/ApiClientCodeGen.VSIX/Windows/AddCustomHeaderDialog.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows.Forms;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Windows
+{
+    [ExcludeFromCodeCoverage]
+    public partial class AddCustomHeaderDialog : Form
+    {
+        public AddCustomHeaderDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string Key => tbKey.Text;
+        public string Value => tbValue.Text;
+
+        private void AddCustomHeaderDialog_Load(object sender, System.EventArgs e)
+        {
+            NativeMethods.SendMessage(
+                tbKey.Handle,
+                NativeMethods.EM_SETCUEBANNER,
+                0,
+                "Authorization");
+
+            NativeMethods.SendMessage(
+                tbValue.Handle,
+                NativeMethods.EM_SETCUEBANNER,
+                0,
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...");
+        }
+
+        private void btnOK_Click(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbKey.Text) || string.IsNullOrWhiteSpace(tbValue.Text))
+            {
+                DialogResult = DialogResult.Cancel;
+                Close();
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, System.EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
