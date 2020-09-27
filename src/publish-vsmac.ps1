5b313ae25929c5b34486c4c12749ffa707a38f3a
Christian Helle <christian.helle@outlook.com>
2020-09-27 17:24:09 +0200

    Fix publish-vsmac.ps1 for re-writing main.mrep

--- src/publish-vsmac.ps1
+++ src/publish-vsmac.ps1
@@ -5,6 +5,13 @@ $resultRoot += (Invoke-RestMethod 'https://christianhelle.com/vsmac/root.mrep').
 $resultRoot += '</Repository>'
 ([xml]$resultRoot).Save("$pwd\root.mrep")
 
-$main = Invoke-RestMethod 'https://christianhelle.com/vsmac/main.mrep'
-$main.Repository.Repository.LastModified = (Get-Date).ToString('o')
+$main = '<?xml version="1.0" encoding="utf-8"?>'
+$main += '<Repository xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+$main += '<Repository>'
+$main += '<Url>root.mrep</Url>'
+$main += '<LastModified>'
+$main += (Get-Date).ToString('o')
+$main += '</LastModified>'
+$main += '</Repository>'
+$main += '</Repository>'
 ([xml]$main).Save("$pwd\main.mrep")
