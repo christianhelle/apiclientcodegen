af2e4c842d3dffce02f02576ec0e15fe533d401f
Christian Helle <christian.helle@outlook.com>
2020-10-17 16:12:17 +0200

    Create SupportedCodeGenerator extension methods

---
+++ src/ApiClientCodeGen.Core/Extensions/SupportedCodeGeneratorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.NuGet;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions
+{
+    public static class SupportedCodeGeneratorExtensions
+    {
+        private static readonly PackageDependencyListProvider DependencyListProvider
+            = new PackageDependencyListProvider();
+
+        public static IEnumerable<PackageDependency> GetDependencies(
+            this SupportedCodeGenerator generator)
+            => DependencyListProvider
+                .GetDependencies(generator);
+    }
+}
