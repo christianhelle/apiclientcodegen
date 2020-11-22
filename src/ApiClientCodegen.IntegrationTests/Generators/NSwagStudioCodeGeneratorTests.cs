fbefeb97a8c38431c828b4bf31e87c5ffc41aab5
Christian Helle <christian.helle@outlook.com>
2020-11-22 16:02:49 +0100

    Fix incorrect asserts

--- src/ApiClientCodegen.IntegrationTests/Generators/NSwagStudioCodeGeneratorTests.cs
+++ src/ApiClientCodegen.IntegrationTests/Generators/NSwagStudioCodeGeneratorTests.cs
@@ -32,7 +32,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
             => new NSwagStudioCodeGenerator(Path.GetFullPath(SwaggerNSwagFilename), options, new ProcessLauncher())
                 .GenerateCode(new Mock<IProgressReporter>().Object)
                 .Should()
-                .BeNull();
+                .NotBeNullOrWhiteSpace();
         
         [Xunit.Fact]
         public async Task NSwagStudio_Generate_Code_Using_NSwagStudio_From_SwaggerSpec()
@@ -45,7 +45,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
             new NSwagStudioCodeGenerator(Path.GetFullPath("Petstore.nswag"), options, new ProcessLauncher())
                 .GenerateCode(new Mock<IProgressReporter>().Object)
                 .Should()
-                .BeNull();
+                .NotBeNullOrWhiteSpace();
 
             File.Exists(Path.GetFullPath("PetstoreClient.cs"))
                 .Should()
