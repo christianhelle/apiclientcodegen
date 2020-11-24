d0ef51538dcc76c419b3b7937bc99d07fe1239fa
Christian Helle <christian.helle@outlook.com>
2020-11-24 13:45:06 +0100

    Remove unused scripts

--- src/integration-tests.sh
+++
@@ -1,9 +0,0 @@
-dotnet tool install --global rapicgen
-export PATH="$PATH:/Users/runner/.dotnet/tools"
-
-pwsh -Command Invoke-WebRequest -Uri https://petstore.swagger.io/v2/swagger.json -OutFile Swagger.json
-
-rapicgen autorest Swagger.json GeneratedCode ./AutoRestOutput.cs
-rapicgen nswag Swagger.json GeneratedCode ./NSwagOutput.cs
-rapicgen swagger Swagger.json GeneratedCode ./SwaggerOutput.cs
-rapicgen openapi Swagger.json GeneratedCode ./OpenApiOutput.cs
