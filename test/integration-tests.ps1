a0f40003276a7d76eced59f250278241780f761e
Christian Helle <christian.helle@outlook.com>
2020-11-24 13:45:42 +0100

    Update integration test script to build generated code

--- test/integration-tests.ps1
+++ test/integration-tests.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 Write-Host "`r`nInstall .NET Core Tool`r`n"
 dotnet tool update --global rapicgen
 
@@ -5,18 +7,21 @@ Write-Host "`r`nDownload Swagger Petstore spec`r`n"
 Invoke-WebRequest -Uri https://petstore.swagger.io/v2/swagger.json -OutFile Swagger.json
 
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-rapicgen autorest Swagger.json GeneratedCode ./AutoRestOutput.cs
+rapicgen autorest ./Swagger.json GeneratedCode ./GeneratedCode/AutoRest/AutoRestOutput.cs
+dotnet build ./GeneratedCode/AutoRest/Project.csproj
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-rapicgen nswag Swagger.json GeneratedCode ./NSwagOutput.cs
+rapicgen nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/NSwagOutput.cs
+dotnet build ./GeneratedCode/NSwag/Project.csproj
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-rapicgen swagger Swagger.json GeneratedCode ./SwaggerOutput.cs
+rapicgen swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/SwaggerOutput.cs
+dotnet build ./GeneratedCode/SwaggerCodegen/Project.csproj
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-rapicgen openapi Swagger.json GeneratedCode ./OpenApiOutput.cs
+rapicgen openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/OpenApiOutput.cs
+dotnet build ./GeneratedCode/OpenApiGenerator/Project.csproj
 
-rm Swagger.json
-rm ./*.cs
+Remove-Item Swagger.json
 
 Write-Host "`r`n"
