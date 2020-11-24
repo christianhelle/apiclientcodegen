ac4e1075d237b42545475e4212dac1666238b6ef
Christian Helle <christian.helle@outlook.com>
2020-11-24 13:24:50 +0100

    Update smoke test script to build output files

--- test/smoke-tests.ps1
+++ test/smoke-tests.ps1
@@ -2,19 +2,21 @@ Write-Host "`r`nDownload Swagger Petstore spec`r`n"
 Invoke-WebRequest -Uri https://petstore.swagger.io/v2/swagger.json -OutFile Swagger.json
 
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.json GeneratedCode ./AutoRestOutput.cs
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.json GeneratedCode ./GeneratedCode/AutoRest/AutoRestOutput.cs
+dotnet build ./GeneratedCode/AutoRest/Project.csproj
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./NSwagOutput.cs
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/NSwagOutput.cs
+dotnet build ./GeneratedCode/NSwag/Project.csproj
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./SwaggerOutput.cs
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/SwaggerOutput.cs
+dotnet build ./GeneratedCode/SwaggerCodegen/Project.csproj
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./OpenApiOutput.cs
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/OpenApiOutput.cs
+dotnet build ./GeneratedCode/OpenApiGenerator/Project.csproj
 
-
-rm Swagger.json
-rm ./*.cs
+Remove-Item Swagger.json
 
 Write-Host "`r`n"
