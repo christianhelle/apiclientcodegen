b9d9a9e2964c7a6c976bd6bbd5ff3371f052b99b
Christian Helle <christian.helle@outlook.com>
2020-11-29 15:30:29 +0100

    Create copy of DependencyDownloaderTests in core integration test project

---
+++ src/ApiClientCodeGen.Core.IntegrationTests/DependencyDownloaderTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using ApiClientCodeGen.Tests.Common;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests
+{
+    public class DependencyDownloaderTests : TestWithResources
+    {
+        [Fact]
+        public void InstallOpenApiGenerator_Returns_Path()
+            => DependencyDownloader
+                .InstallOpenApiGenerator()
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [Fact]
+        public void InstallSwaggerCodegenCli_Returns_Path()
+            => DependencyDownloader
+                .InstallSwaggerCodegenCli()
+                .Should()
+                .NotBeNullOrWhiteSpace();
+                
+        [Fact]
+        public void InstallOpenApiGenerator_With_Path_Returns_Path()
+            => DependencyDownloader
+                .InstallOpenApiGenerator(Path.GetTempPath())
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [Fact]
+        public void InstallSwaggerCodegenCli_With_Path_Returns_Path()
+            => DependencyDownloader
+                .InstallSwaggerCodegenCli(Path.GetTempPath())
+                .Should()
+                .NotBeNullOrWhiteSpace();
+        
+        [Fact]
+        public void InstallOpenApiGenerator_Force_Returns_Path()
+            => DependencyDownloader
+                .InstallOpenApiGenerator(forceDownload: true)
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [Fact]
+        public void InstallSwaggerCodegenCli_Force_Returns_Path()
+            => DependencyDownloader
+                .InstallSwaggerCodegenCli(forceDownload: true)
+                .Should()
+                .NotBeNullOrWhiteSpace();
+
+        [Fact]
+        public void InstallAutoRest_Returns_Path()
+            => new Action(DependencyDownloader.InstallAutoRest)
+                .Should()
+                .NotThrow();
+
+        [Fact]
+        public void InstallNSwag_Returns_Path()
+            => new Action(DependencyDownloader.InstallNSwag)
+                .Should()
+                .NotThrow();
+    }
+}
