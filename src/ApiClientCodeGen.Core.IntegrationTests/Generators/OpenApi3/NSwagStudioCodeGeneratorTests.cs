f85522267da37b418ffb4be07a81566c8257bf80
Christian Resma Helle <christian.helle@outlook.com>
2020-11-29 17:22:48 +0100

    Remove invalid test

--- src/ApiClientCodeGen.Core.IntegrationTests/Generators/OpenApi3/NSwagStudioCodeGeneratorTests.cs
+++ src/ApiClientCodeGen.Core.IntegrationTests/Generators/OpenApi3/NSwagStudioCodeGeneratorTests.cs
@@ -1,86 +1,26 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using ApiClientCodeGen.Tests.Common;
+﻿using ApiClientCodeGen.Tests.Common.Build;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwagStudio;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwagStudio;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.NSwagStudio;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Windows;
-using FluentAssertions;
-using Moq;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.OpenApi3;
+using Xunit;
 
 namespace ApiClientCodeGen.Core.IntegrationTests.Generators.OpenApi3
 {
-    
-    [Xunit.Trait("Category", "SkipWhenLiveUnitTesting")]
-    public class NSwagStudioCodeGeneratorTests : TestWithResources
+    [Trait("Category", "SkipWhenLiveUnitTesting")]
+    public class NSwagStudioCodeGeneratorTests : IClassFixture<NSwagStudioCodeGeneratorFixture>
     {
-        private Mock<IGeneralOptions> optionsMock;
-        private IGeneralOptions options;
+        private readonly string code;
 
-        public NSwagStudioCodeGeneratorTests()
+        public NSwagStudioCodeGeneratorTests(NSwagStudioCodeGeneratorFixture fixture)
         {
-            optionsMock = new Mock<IGeneralOptions>();
-            optionsMock.Setup(c => c.NSwagPath).Returns(PathProvider.GetNSwagPath());
-            options = optionsMock.Object;
+            code = fixture.Code;
         }
 
-        [Xunit.Fact]
-        public void NSwagStudio_Generate_Code_Using_NSwagStudio()
-            => new NSwagStudioCodeGenerator(Path.GetFullPath(SwaggerV3NSwagFilename), options, new ProcessLauncher())
-                .GenerateCode(new Mock<IProgressReporter>().Object)
-                .Should()
-                .BeNull();
-        
-        [Xunit.Fact]
-        public async Task NSwagStudio_Generate_Code_Using_NSwagStudio_From_SwaggerSpec()
-        {
-            var options = new Mock<INSwagStudioOptions>();
-            options.Setup(c => c.UseDocumentTitle).Returns(false);
-            options.Setup(c => c.GenerateDtoTypes).Returns(true);
+        [Fact]
+        public void GeneratedCode_Can_Build_In_NetCoreApp() 
+            => BuildHelper.BuildCSharp(ProjectTypes.DotNetCoreApp, code, SupportedCodeGenerator.NSwagStudio);
 
-            var outputFilename = $"PetstoreClient{Guid.NewGuid():N}";
-            var contents = await NSwagStudioFileHelper.CreateNSwagStudioFileAsync(
-                new EnterOpenApiSpecDialogResult(
-                    ReadAllText(SwaggerV3Json),
-                    outputFilename,
-                    "https://petstore.swagger.io/v2/swagger.json"),
-                options.Object);
-
-            var tempFile = Path.GetTempFileName();
-            File.WriteAllText(tempFile, contents);
-
-            new NSwagStudioCodeGenerator(tempFile, optionsMock.Object, new ProcessLauncher())
-                .GenerateCode(new Mock<IProgressReporter>().Object)
-                .Should()
-                .BeNull();
-        }
-
-        [Xunit.Fact]
-        public void Reads_NSwagPath_From_Options()
-        {
-            var tempFile = Path.GetTempFileName();
-            File.WriteAllText(tempFile, ReadAllText(SwaggerV3Json));
-            new NSwagStudioCodeGenerator(
-                    tempFile, 
-                    options,
-                    new ProcessLauncher())
-                .GenerateCode(new Mock<IProgressReporter>().Object);
-
-            optionsMock.Verify(c => c.NSwagPath);
-        }
-
-        [Xunit.Fact]
-        public void GetNSwagPath_ForceDownload()
-            => new NSwagStudioCodeGenerator(
-                    Path.GetFullPath(SwaggerV3NSwagFilename),
-                    options,
-                    new ProcessLauncher())
-                .GetNSwagPath()
-                .Should()
-                .NotBeNullOrWhiteSpace();
+        [Fact]
+        public void GeneratedCode_Can_Build_In_NetStandardLibrary() 
+            => BuildHelper.BuildCSharp(ProjectTypes.DotNetStandardLibrary, code, SupportedCodeGenerator.NSwagStudio);
     }
 }
