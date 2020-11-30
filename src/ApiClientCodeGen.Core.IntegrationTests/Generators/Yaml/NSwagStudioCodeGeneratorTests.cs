7d2f9ec0cae5877aee58203d51e38a33a9ac10b7
Christian Helle <crh@delegate.dk>
2020-11-30 10:16:05 +0100

    Add missing NSwag Studio integration tests for YAML files

---
+++ src/ApiClientCodeGen.Core.IntegrationTests/Generators/Yaml/NSwagStudioCodeGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using ApiClientCodeGen.Tests.Common.Build;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.Yaml;
+using Xunit;
+
+namespace ApiClientCodeGen.Core.IntegrationTests.Generators.Yaml
+{
+    [Trait("Category", "SkipWhenLiveUnitTesting")]
+    public class NSwagStudioCodeGeneratorTests : IClassFixture<NSwagStudioCodeGeneratorFixture>
+    {
+        private readonly string code;
+
+        public NSwagStudioCodeGeneratorTests(NSwagStudioCodeGeneratorFixture fixture)
+        {
+            code = fixture.Code;
+        }
+
+        [Fact]
+        public void GeneratedCode_Can_Build_In_NetCoreApp() 
+            => BuildHelper.BuildCSharp(ProjectTypes.DotNetCoreApp, code, SupportedCodeGenerator.NSwagStudio);
+
+        [Fact]
+        public void GeneratedCode_Can_Build_In_NetStandardLibrary() 
+            => BuildHelper.BuildCSharp(ProjectTypes.DotNetStandardLibrary, code, SupportedCodeGenerator.NSwagStudio);
+    }
+}
