b941868e0d9904fbab2a5faa19025b7b75d4fa02
Christian Helle <christian.helle@outlook.com>
2020-12-06 23:35:23 +0100

    Add new smoke test project types to script

--- test/smoke-tests.ps1
+++ test/smoke-tests.ps1
@@ -1,4 +1,12 @@
+Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
 
 Invoke-WebRequest -Uri https://dotnet.microsoft.com/download/dotnet-core/scripts/v1/dotnet-install.ps1 -OutFile ./dotnet-install.ps1
 ./dotnet-install.ps1 -Version 2.1.811
@@ -15,28 +23,36 @@ Write-Host "`r`nDownload Swagger Petstore V2 spec (JSON)`r`n"
 Invoke-WebRequest -Uri https://petstore.swagger.io/v2/swagger.json -OutFile Swagger.json
 
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.json GeneratedCode ./GeneratedCode/AutoRest/Output.cs
-dotnet build ./GeneratedCode/AutoRest/NetCore21.csproj
-dotnet build ./GeneratedCode/AutoRest/NetCore31.csproj
-dotnet build ./GeneratedCode/AutoRest/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.json GeneratedCode ./GeneratedCode/AutoRest/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/Output.cs
-dotnet build ./GeneratedCode/NSwag/NetCore21.csproj
-dotnet build ./GeneratedCode/NSwag/NetCore31.csproj
-dotnet build ./GeneratedCode/NSwag/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetStandard20.csproj; ThrowOnNativeFailure
 
 Remove-Item Swagger.json
 Remove-Item ./**/*Output.cs
@@ -49,28 +65,36 @@ Write-Host "`r`nDownload Swagger Petstore V2 spec (YAML)`r`n"
 Invoke-WebRequest -Uri https://petstore.swagger.io/v2/swagger.yaml -OutFile Swagger.yaml
 
 Write-Host "`r`nTesting AutoRest Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.yaml GeneratedCode ./GeneratedCode/AutoRest/Output.cs
-dotnet build ./GeneratedCode/AutoRest/NetCore21.csproj
-dotnet build ./GeneratedCode/AutoRest/NetCore31.csproj
-dotnet build ./GeneratedCode/AutoRest/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- autorest ./Swagger.yaml GeneratedCode ./GeneratedCode/AutoRest/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/AutoRest/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.yaml GeneratedCode ./GeneratedCode/NSwag/Output.cs
-dotnet build ./GeneratedCode/NSwag/NetCore21.csproj
-dotnet build ./GeneratedCode/NSwag/NetCore31.csproj
-dotnet build ./GeneratedCode/NSwag/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.yaml GeneratedCode ./GeneratedCode/NSwag/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.yaml GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.yaml GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.yaml GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.yaml GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetStandard20.csproj; ThrowOnNativeFailure
 
 Remove-Item Swagger.yaml
 Remove-Item ./**/*Output.cs
@@ -83,22 +107,28 @@ Write-Host "`r`nDownload Swagger Petstore V3 spec (JSON)`r`n"
 Invoke-WebRequest -Uri https://petstore3.swagger.io/api/v3/openapi.json -OutFile Swagger.json
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/Output.cs
-dotnet build ./GeneratedCode/NSwag/NetCore21.csproj
-dotnet build ./GeneratedCode/NSwag/NetCore31.csproj
-dotnet build ./GeneratedCode/NSwag/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.json GeneratedCode ./GeneratedCode/NSwag/Output.cs; ThrowOnNativeFailure; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.json GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.json GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetStandard20.csproj; ThrowOnNativeFailure
 
 Remove-Item Swagger.json
 Remove-Item ./**/*Output.cs
@@ -111,22 +141,28 @@ Write-Host "`r`nDownload Swagger Petstore V3 spec (YAML)`r`n"
 Invoke-WebRequest -Uri https://petstore3.swagger.io/api/v3/openapi.yaml -OutFile Swagger.yaml
 
 Write-Host "`r`nTesting NSwag Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.yaml GeneratedCode ./GeneratedCode/NSwag/Output.cs
-dotnet build ./GeneratedCode/NSwag/NetCore21.csproj
-dotnet build ./GeneratedCode/NSwag/NetCore31.csproj
-dotnet build ./GeneratedCode/NSwag/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- nswag ./Swagger.yaml GeneratedCode ./GeneratedCode/NSwag/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/NSwag/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Swagger Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.yaml GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj
-dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- swagger ./Swagger.yaml GeneratedCode ./GeneratedCode/SwaggerCodegen/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/SwaggerCodegen/NetStandard20.csproj; ThrowOnNativeFailure
 
 Write-Host "`r`nTesting Open API Code Generation`r`n"
-dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.yaml GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj
-dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj
+dotnet run --project ../src/ApiClientCodeGen.CLI/ApiClientCodeGen.CLI.csproj -- openapi ./Swagger.yaml GeneratedCode ./GeneratedCode/OpenApiGenerator/Output.cs; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore21.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetCore31.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net5.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/Net472.csproj; ThrowOnNativeFailure
+dotnet build ./GeneratedCode/OpenApiGenerator/NetStandard20.csproj; ThrowOnNativeFailure
 
 Remove-Item Swagger.yaml
 Remove-Item ./**/*Output.cs
