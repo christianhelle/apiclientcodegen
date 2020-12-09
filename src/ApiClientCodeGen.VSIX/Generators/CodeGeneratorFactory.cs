09b79c2c830afcac3dc2c434282f881720811413
Christian Helle <christian.helle@outlook.com>
2020-12-09 22:35:24 +0100

    Track code generator usage in CodeGeneratorFactory

--- src/ApiClientCodeGen.VSIX/Generators/CodeGeneratorFactory.cs
+++ src/ApiClientCodeGen.VSIX/Generators/CodeGeneratorFactory.cs
@@ -1,37 +1,42 @@
 ﻿using System;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.AutoRest;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.Swagger;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.NSwag;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.AutoRest;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.General;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.NSwag;
-using OpenApiDocumentFactory = ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.NSwag.OpenApiDocumentFactory;
+using OpenApiDocumentFactory =
+    ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.NSwag.OpenApiDocumentFactory;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators
 {
     public class CodeGeneratorFactory : ICodeGeneratorFactory
     {
+        private readonly ILogger logger;
         private readonly IOpenApiDocumentFactory documentFactory;
         private readonly IProcessLauncher processLauncher;
         private readonly IOptionsFactory optionsFactory;
 
         public CodeGeneratorFactory(
-            IOptionsFactory optionsFactory = null, 
+            IOptionsFactory optionsFactory = null,
             IProcessLauncher processLauncher = null,
-            IOpenApiDocumentFactory documentFactory = null)
+            IOpenApiDocumentFactory documentFactory = null,
+            ILogger logger = null)
         {
             this.optionsFactory = optionsFactory ?? new OptionsFactory();
             this.processLauncher = processLauncher ?? new ProcessLauncher();
             this.documentFactory = documentFactory ?? new OpenApiDocumentFactory();
+            this.logger = logger ?? Logger.Instance;
         }
 
         public ICodeGenerator Create(
@@ -41,6 +46,10 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators
             SupportedLanguage language,
             SupportedCodeGenerator generator)
         {
+            logger.TrackFeatureUsage(
+                generator.GetName(),
+                "VSIX");
+
             switch (generator)
             {
                 case SupportedCodeGenerator.AutoRest:
