12a8336f7b7ecb3dc97ac8b55644747571cc6302
Christian Helle <christian.helle@outlook.com>
2020-12-14 21:00:19 +0100

    Add [CLI] prefix to Sentry entries

--- src/ApiClientCodeGen.CLI/SentryRemoteLogger.cs
+++ src/ApiClientCodeGen.CLI/SentryRemoteLogger.cs
@@ -17,12 +17,12 @@ namespace ApiClientCodeGen.CLI
                 return;
             SentrySdk.Init("https://c925c0d0b9ea438a865377e066eeb8ff@o418075.ingest.sentry.io/5555548");
         }
-        
+
         public void TrackFeatureUsage(string featureName, params string[] tags)
         {
             if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
                 return;
-            SentrySdk.CaptureMessage($"[FEATURE USAGE] {featureName}", SentryLevel.Debug);
+            SentrySdk.CaptureMessage($"[CLI] {featureName}", SentryLevel.Debug);
         }
 
         public void TrackError(Exception exception)
