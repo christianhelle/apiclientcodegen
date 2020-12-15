3853526a3caa7ad8a65100e356c4f02c1925c2f4
Christian Helle <christian.helle@outlook.com>
2020-12-15 10:36:56 +0100

    Use GetName() instead of GetCustomToolName()

--- src/ApiClientCodeGen.VSIX/Commands/AddNew/NewRestClientCommand.cs
+++ src/ApiClientCodeGen.VSIX/Commands/AddNew/NewRestClientCommand.cs
@@ -3,12 +3,12 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
-using System.Windows.Forms;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwagStudio;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.General;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.NSwagStudio;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Windows;
@@ -34,7 +34,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Commands.AddNew
         private async Task OnExecuteAsync(DTE dte, AsyncPackage package)
         {
             Logger.Instance.TrackFeatureUsage(
-                $"New REST API Client ({CodeGenerator.GetCustomToolName()})",
+                $"New REST API Client ({CodeGenerator.GetName()})",
                 "VSIX");
 
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
