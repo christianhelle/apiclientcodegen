25cdd0e9a44ccdd968b2557a185a2bea760784cc
Christian Helle <christian.helle@outlook.com>
2020-12-15 23:37:02 +0100

    Implement IEventPlugin for ignoring non-project related exceptions in Exceptionless

--- src/ApiClientCodeGen.Core/Logging/ExceptionlessRemoteLogger.cs
+++ src/ApiClientCodeGen.Core/Logging/ExceptionlessRemoteLogger.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Exceptionless;
+using Exceptionless.Plugins;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
 {
@@ -12,6 +15,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
         {
             if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
                 return;
+            ExceptionlessClient.Default.Configuration.AddPlugin<IgnoreNonProjectReletedExceptionsPlugin>();
             ExceptionlessClient.Default.Startup("6CRkH7zip11qalrUJgxi78lVyi93rxhQkzbYZfK2");
         }
 
@@ -30,6 +34,35 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
             if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
                 return;
             exception.ToExceptionless().Submit();
+        }
+
+        [Priority(30)]
+        private class IgnoreNonProjectReletedExceptionsPlugin : IEventPlugin
+        {
+            private static readonly List<string> HandledNamespaces = new List<string>
+            {
+                "ChristianHelle",
+                "ApiClientCodeGen"
+            };
+
+            public void Run(EventPluginContext ctx)
+            {
+                if (!ctx.ContextData.IsUnhandledError || !ctx.Event.IsError() || !ctx.ContextData.HasException())
+                    return;
+
+                var exception = ctx.ContextData.GetException();
+                if (exception == null)
+                    return;
+
+                var error = ctx.Event.GetError();
+                if (error == null)
+                    return;
+
+                ctx.Cancel = !error.StackTrace
+                    .Select(s => s.DeclaringNamespace)
+                    .Distinct()
+                    .Any(ns => HandledNamespaces.Any(ns.Contains));
+            }
         }
     }
 }
