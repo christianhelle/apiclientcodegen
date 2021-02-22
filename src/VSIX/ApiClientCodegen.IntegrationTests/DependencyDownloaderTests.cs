33eb80ef9642a76c71b8e406bc7e54ad3360865a
Christian Helle <crh@delegate.dk>
2021-02-22 16:36:22 +0100

    Fix missing changes after refactoring

--- src/VSIX/ApiClientCodegen.IntegrationTests/DependencyDownloaderTests.cs
+++ src/VSIX/ApiClientCodegen.IntegrationTests/DependencyDownloaderTests.cs
@@ -52,8 +52,14 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
                 .NotBeNullOrWhiteSpace();
 
         [Fact]
-        public void InstallAutoRest_Returns_Path()
-            => new Action(DependencyDownloader.InstallAutoRest)
+        public void InstallAutoRestV2_Returns_Path()
+            => new Action(DependencyDownloader.InstallAutoRestV2)
+                .Should()
+                .NotThrow();
+
+        [Fact]
+        public void InstallAutoRestV3_Returns_Path()
+            => new Action(DependencyDownloader.InstallAutoRestV3)
                 .Should()
                 .NotThrow();
 
