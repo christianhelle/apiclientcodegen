6c10eca8acb0b840b3440561a030a770913ae07a
Christian Helle <christian.helle@outlook.com>
2021-03-06 14:48:31 +0100

    Move implementation to separate file

--- src/Core/ApiClientCodeGen.Core/Installer/IDependencyInstaller.cs
+++ src/Core/ApiClientCodeGen.Core/Installer/IDependencyInstaller.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer
@@ -9,41 +8,5 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer
         Task InstallNSwag();
         Task InstallOpenApiGenerator();
         Task InstallSwaggerCodegen();
-    }
-
-    public class DependencyInstaller : IDependencyInstaller
-    {
-        private readonly INpmInstaller npm;
-        private readonly IFileDownloader downloader;
-
-        public DependencyInstaller(INpmInstaller npm, IFileDownloader downloader)
-        {
-            this.npm = npm ?? throw new ArgumentNullException(nameof(npm));
-            this.downloader = downloader ?? throw new ArgumentNullException(nameof(downloader));
-        }
-
-        public Task InstallAutoRest()
-        {
-            return npm.InstallNpmPackage("autorest");
-        }
-
-        public Task InstallNSwag()
-        {
-            return npm.InstallNpmPackage("nswag");
-        }
-
-        public Task InstallOpenApiGenerator()
-        {
-            return downloader.DownloadFile(
-                null,
-                "openapi-generator-cli.jar",
-                Resource.OpenApiGenerator_MD5,
-                Resource.OpenApiGenerator_DownloadUrl);
-        }
-
-        public Task InstallSwaggerCodegen()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
