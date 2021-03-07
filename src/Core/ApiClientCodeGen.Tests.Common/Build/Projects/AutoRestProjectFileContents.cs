dabb416a7c46e446d5ebb2de55a80202fc61334f
Christian Helle <christian.helle@outlook.com>
2021-03-07 13:32:05 +0100

    Set <LangVersion> to 8.0 in AutoRest test projects

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/AutoRestProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/AutoRestProjectFileContents.cs
@@ -6,6 +6,7 @@
             @"<Project Sdk=""Microsoft.NET.Sdk"">
   <PropertyGroup>
     <TargetFramework>netcoreapp2.1</TargetFramework>
+    <LangVersion>8.0</LangVersion>
     <IncludeGeneratorSharedCode>true</IncludeGeneratorSharedCode>
     <RestoreAdditionalProjectSources>https://azuresdkartifacts.blob.core.windows.net/azure-sdk-tools/index.json</RestoreAdditionalProjectSources>
   </PropertyGroup>
@@ -23,6 +24,7 @@
             @"<Project Sdk=""Microsoft.NET.Sdk"">
   <PropertyGroup>
     <TargetFramework>netstandard2.0</TargetFramework>
+    <LangVersion>8.0</LangVersion>
     <IncludeGeneratorSharedCode>true</IncludeGeneratorSharedCode>
     <RestoreAdditionalProjectSources>https://azuresdkartifacts.blob.core.windows.net/azure-sdk-tools/index.json</RestoreAdditionalProjectSources>
   </PropertyGroup>
