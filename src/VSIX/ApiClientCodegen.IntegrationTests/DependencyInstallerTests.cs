8fbe2b8ccee06a41fe1ba05a0be6d13c4acd5478
Christian Helle <christian.helle@outlook.com>
2021-03-07 13:53:39 +0100

    Rename file

--- src/VSIX/ApiClientCodegen.IntegrationTests/DependencyInstallerTests.cs
+++ src/VSIX/ApiClientCodegen.IntegrationTests/DependencyInstallerTests.cs
@@ -11,7 +11,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
     public class DependencyInstallerTests : TestWithResources
     {
         [Fact]
-        public async Task InstallOpenApiGenerator_Returns_Path()
+        public async Task InstallOpenApiGenerator_Returns_Path_Async()
             => (await new DependencyInstaller(
                         new NpmInstaller(new ProcessLauncher()),
                         new FileDownloader(new WebDownloader()))
@@ -20,7 +20,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
                 .NotBeNullOrWhiteSpace();
 
         [Fact]
-        public async Task InstallSwaggerCodegen_Returns_Path()
+        public async Task InstallSwaggerCodegen_Returns_Path_Async()
             => (await new DependencyInstaller(
                         new NpmInstaller(new ProcessLauncher()),
                         new FileDownloader(new WebDownloader()))
@@ -29,21 +29,25 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTest
                 .NotBeNullOrWhiteSpace();
 
         [Fact]
-        public void InstallAutoRest_Returns_Path()
-            => new Func<Task>(
-                    async () => await new DependencyInstaller(
-                        new NpmInstaller(new ProcessLauncher()),
-                        new FileDownloader(new WebDownloader())).InstallAutoRest())
-                .Should()
-                .NotThrow();
+        public async Task InstallAutoRest_Returns_Path_Async()
+        {
+            var func = new Func<Task>(
+                () => new DependencyInstaller(
+                    new NpmInstaller(new ProcessLauncher()),
+                    new FileDownloader(new WebDownloader())).InstallAutoRest());
+            await func();
+            func.Should().NotThrow();
+        }
 
         [SkippableFact(typeof(ProcessLaunchException))]
-        public void InstallNSwag_Returns_Path()
-            => new Func<Task>(
-                    async () => await new DependencyInstaller(
-                        new NpmInstaller(new ProcessLauncher()),
-                        new FileDownloader(new WebDownloader())).InstallNSwag())
-                .Should()
-                .NotThrow();
+        public async Task InstallNSwag_Returns_Path_Async()
+        {
+            var func = new Func<Task>(
+                () => new DependencyInstaller(
+                    new NpmInstaller(new ProcessLauncher()),
+                    new FileDownloader(new WebDownloader())).InstallNSwag());
+            await func();
+            func.Should().NotThrow();
+        }
     }
 }
