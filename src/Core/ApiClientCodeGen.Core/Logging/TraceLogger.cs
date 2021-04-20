b1f4e180869510b341724f13535fb095b7f5552d
Christian Helle <christian.helle@outlook.com>
2021-04-20 09:21:42 +0200

    Introduce ITraceLogger

---
+++ src/Core/ApiClientCodeGen.Core/Logging/TraceLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using SystemTrace = System.Diagnostics.Trace;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
+{
+    [ExcludeFromCodeCoverage]
+    public static class TraceLogger
+    {
+        private static readonly List<ITraceLogger> Loggers = new List<ITraceLogger>
+        {
+            new SystemTraceLogger()
+        };
+
+        public static void Setup(params ITraceLogger[] loggers)
+        {
+            Loggers.AddRange(loggers);
+        }
+        
+        public static void Write(string message) 
+            => Loggers.ForEach(c => c.WriteLine(message));
+
+        public static void WriteLine(string message)
+        => Loggers.ForEach(c => c.WriteLine(message));
+
+        public static void Write(Exception exception)
+            => Loggers.ForEach(c => c.Write(exception.ToString()));
+    }
+
+    public interface ITraceLogger
+    {
+        void Write(string message);
+        void WriteLine(string message);
+        void Write(Exception exception);
+    }
+
+    public class SystemTraceLogger : ITraceLogger
+    {
+        public void Write(string message)
+            => SystemTrace.WriteLine(message);
+
+        public void WriteLine(string message)
+            => SystemTrace.WriteLine(message);
+
+        public void Write(Exception exception)
+            => SystemTrace.TraceError(exception.ToString());
+    }
+}
