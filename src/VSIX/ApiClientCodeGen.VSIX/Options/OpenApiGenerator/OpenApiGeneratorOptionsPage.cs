603d08e53abb066e0d050f861e36c4251437ad34
Christian Helle <christian.helle@outlook.com>
2021-08-13 21:06:37 +0200

    Update EmitDefaultValue description

--- src/VSIX/ApiClientCodeGen.VSIX/Options/OpenApiGenerator/OpenApiGeneratorOptionsPage.cs
+++ src/VSIX/ApiClientCodeGen.VSIX/Options/OpenApiGenerator/OpenApiGeneratorOptionsPage.cs
@@ -9,10 +9,13 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.OpenApi
     public class OpenApiGeneratorOptionsPage : DialogPage, IOpenApiGeneratorOptions
     {
         public const string Name = "OpenAPI Generator";
-        
+
         [Category(Name)]
         [DisplayName("Emit Default Value")]
-        [Description("Set to true if the default value for a member should be generated in the serialization stream")]
-        public bool EmitDefaultValue { get; set; }
+        [Description("Set to true if the default value for a member should be generated in the serialization stream. " +
+                     "Setting the EmitDefaultValue property to false is not a recommended practice. " +
+                     "It should only be done if there is a specific need to do so " +
+                     "(such as for interoperability or to reduce data size).")]
+        public bool EmitDefaultValue { get; set; } = true;
     }
 }
