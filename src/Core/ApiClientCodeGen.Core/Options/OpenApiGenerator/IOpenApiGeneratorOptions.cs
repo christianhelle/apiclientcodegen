aae144c956f70c051e9136e119a5160e0b81b6f2
Christian Helle <christian.helle@outlook.com>
2021-08-13 13:03:14 +0200

    Introduce IOpenApiGeneratorOptions

---
+++ src/Core/ApiClientCodeGen.Core/Options/OpenApiGenerator/IOpenApiGeneratorOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.OpenApiGenerator
+{
+    public interface IOpenApiGeneratorOptions
+    {
+        bool EmitDefaultValue  { get; }
+    }
+}
