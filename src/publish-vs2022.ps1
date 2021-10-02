a13f452bf90fbfb4b0dea6aa2276d709f6e60df2
Christian Helle <christian.helle@outlook.com>
2021-10-03 00:26:29 +0200

    Update VS Marketplace publishing script

--- src/publish-vs2022.ps1
+++ src/publish-vs2022.ps1
@@ -1,15 +1,26 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $personalAccessToken,
+    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $version
+)
+
 $VisualStudioVersion = "15.0";
 $VSINSTALLDIR =  $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion;
 $VSIXPublisherPath = $VSINSTALLDIR + "VSSDK\VisualStudioIntegration\Tools\Bin\"
-$DropFolder = "$(System.DefaultWorkingDirectory)/CI Build/drop"
-$VSIXFileName = $DropFolder + "/" + (Get-ChildItem -Path $DropFolder -Filter "ApiClientCodeGenerator-VS2022-*.vsix" | Select-Object -First 1).Name
+$VSIXFileName = (Get-ChildItem -Path . -Filter "ApiClientCodeGenerator-VS2022-$version.vsix" | Select-Object -First 1).Name
 $env:Path += ";$VSIXPublisherPath"
 
 VsixPublisher.exe login `
-    -personalAccessToken '$(PersonalAccessToken)' `
-    -publisherName 'ChristianResmaHelle'
+    -personalAccessToken $personalAccessToken `
+    -publisherName ChristianResmaHelle
 
 VsixPublisher.exe publish `
     -payload $VSIXFileName `
-    -publishManifest $DropFolder/publish-manifest-vs2022.json `
+    -publishManifest publish-manifest-vs2022.json `
     -ignoreWarnings 'VSIXValidatorWarning01,VSIXValidatorWarning02'
--- src/publish.ps1
+++ src/publish.ps1
@@ -1,15 +1,26 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $personalAccessToken,
+    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $version
+)
+
 $VisualStudioVersion = "15.0";
 $VSINSTALLDIR =  $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion;
 $VSIXPublisherPath = $VSINSTALLDIR + "VSSDK\VisualStudioIntegration\Tools\Bin\"
-$DropFolder = "$(System.DefaultWorkingDirectory)/CI Build/drop"
-$VSIXFileName = $DropFolder + "/" + (Get-ChildItem -Path $DropFolder -Filter "ApiClientCodeGenerator-*.vsix" | Select-Object -First 1).Name
+$VSIXFileName = (Get-ChildItem -Path . -Filter "ApiClientCodeGenerator-$version.vsix" | Select-Object -First 1).Name
 $env:Path += ";$VSIXPublisherPath"
 
 VsixPublisher.exe login `
-    -personalAccessToken '$(PersonalAccessToken)' `
-    -publisherName 'ChristianResmaHelle'
+    -personalAccessToken $personalAccessToken `
+    -publisherName ChristianResmaHelle
 
 VsixPublisher.exe publish `
     -payload $VSIXFileName `
-    -publishManifest $DropFolder/publish-manifest.json `
+    -publishManifest publish-manifest.json `
     -ignoreWarnings 'VSIXValidatorWarning01,VSIXValidatorWarning02'
