53180cba9afc3ad96076243e8327769e97eaf937
Christian Resma Helle <christian.helle@outlook.com>
2021-10-03 10:08:49 +0200

    Hardode path to Visual Studio 2019 in publish-vs2022.ps1

--- src/publish-vs2022.ps1
+++ src/publish-vs2022.ps1
@@ -10,14 +10,8 @@ param (
     $version
 )
 
-$VisualStudioVersion = "15.0";
-$VSINSTALLDIR =  $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion;
-$VSIXPublisherPath = $VSINSTALLDIR + "VSSDK\VisualStudioIntegration\Tools\Bin\"
 $VSIXFileName = (Get-ChildItem -Path . -Filter "ApiClientCodeGenerator-VS2022-$version.vsix" | Select-Object -First 1).Name
-$env:GITHUB_PATH += ";$VSIXPublisherPath"
-
-Write-Host $VSIXPublisherPath
-$VsixPublisher = $VSIXPublisherPath + "VsixPublisher.exe"
+$VsixPublisher = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe"
 
 & $VsixPublisher login `
     -personalAccessToken $personalAccessToken `
