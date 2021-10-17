dff5f068431cd4760184ab867b87712115e4d409
Christian Helle <christian.helle@outlook.com>
2021-10-17 14:25:27 +0200

    Update sonar.ps1 to convert .coverage to XML format

--- src/sonar.ps1
+++ src/sonar.ps1
@@ -7,17 +7,22 @@ param (
 
 .\build.ps1 --target Clean
 
-dotnet tool update dotnet-sonarscanner --tool-path .sonar\scanner
+dotnet tool update dotnet-sonarscanner --tool-path .tools\scanner
 
-.\.sonar\scanner\dotnet-sonarscanner begin `
+.\.tools\scanner\dotnet-sonarscanner begin `
     /k:"christianhelle_apiclientcodegen" `
     /o:"christianhelle-github" `
     /d:sonar.login=$sonar `
     /d:sonar.host.url="https://sonarcloud.io" `
     /d:sonar.cs.vstest.reportsPaths=TestResults/**/*.trx `
-    /d:sonar.cs.vscoveragexml.reportsPaths=TestResults/**/*.coverage `
-    /d:sonar.cs.opencover.reportsPaths=**/coverage.cobertura.xml
+    /d:sonar.cs.vscoveragexml.reportsPaths=TestResults/**/*.coveragexml
 
+# Build and Run Tests
 .\build.ps1
 
-.\.sonar\scanner\dotnet-sonarscanner end /d:sonar.login=$sonar
+# Convert .coverage files to XML format
+dotnet tool update dotnet-coverageconverter --tool-path .tools\coverage
+.\.tools\coverage\dotnet-coverageconverter.exe --CoverageFilesFolder "TestResults"
+
+# Publish results to SonarCloud
+.\.tools\scanner\dotnet-sonarscanner end /d:sonar.login=$sonar
