a65d9fab74e490a694d3e0953595c8bd6d7ef1be
Christian Helle <crh@delegate.dk>
2022-01-30 17:58:17 +0100

    Update Polly to v7.2.3 in integration test projects

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/OpenApiGeneratorProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/OpenApiGeneratorProjectFileContents.cs
@@ -10,7 +10,7 @@
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.8.0"" />
     <PackageReference Include = ""RestSharp"" Version=""106.11.7"" />
-    <PackageReference Include = ""Polly"" Version=""7.2.1"" />
+    <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
   </ItemGroup>
@@ -24,7 +24,7 @@
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.8.0"" />
     <PackageReference Include = ""RestSharp"" Version=""106.11.7"" />
-    <PackageReference Include = ""Polly"" Version=""7.2.1"" />
+    <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
   </ItemGroup>
@@ -50,7 +50,7 @@
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.8.0"" />
     <PackageReference Include = ""RestSharp"" Version=""106.11.7"" />
-    <PackageReference Include = ""Polly"" Version=""7.2.1"" />
+    <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
   </ItemGroup>
