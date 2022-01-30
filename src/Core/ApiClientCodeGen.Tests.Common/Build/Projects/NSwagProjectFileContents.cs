e89a19e09318d28bc7dd29d1a8c6c3e6519e7ae4
Christian Helle <crh@delegate.dk>
2022-01-30 18:02:02 +0100

    Update Newtonsoft.Json to v13.0.1 in integration test projects

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/NSwagProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/NSwagProjectFileContents.cs
@@ -8,7 +8,7 @@
     <TargetFramework>netcoreapp2.1</TargetFramework>
   </PropertyGroup>
   <ItemGroup>
-    <PackageReference Include = ""Newtonsoft.Json"" Version=""12.0.3"" />
+    <PackageReference Include = ""Newtonsoft.Json"" Version=""13.0.1"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
   </ItemGroup>
@@ -20,7 +20,7 @@
     <TargetFramework>netstandard2.0</TargetFramework>
   </PropertyGroup>
   <ItemGroup>
-    <PackageReference Include = ""Newtonsoft.Json"" Version=""12.0.3"" />
+    <PackageReference Include = ""Newtonsoft.Json"" Version=""13.0.1"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
   </ItemGroup>
