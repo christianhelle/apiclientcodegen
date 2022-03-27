68e5cf2e66bd8b96cc6a4be7ddd1550e357c79b7
Christian Helle <christian.helle@outlook.com>
2022-03-27 22:42:26 +0200

    Update InstallMissingPackagesAsync() in ProjectExtensions to use "dotnet add package" when running VS 2022

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/Extensions/ProjectExtensions.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/Extensions/ProjectExtensions.cs
@@ -1,21 +1,31 @@
-﻿using System;
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.NuGet;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options.General;
+using Community.VisualStudio.Toolkit;
+using EnvDTE;
+using Microsoft;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using NuGet.VisualStudio;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
+using DTEProject = EnvDTE.Project;
 using Task = System.Threading.Tasks.Task;
+using VSProject = Community.VisualStudio.Toolkit.Project;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
 {
@@ -60,11 +70,70 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
         }
 
         public static async Task InstallMissingPackagesAsync(
-            this Community.VisualStudio.Toolkit.Project project,
+            this VSProject project,
             SupportedCodeGenerator codeGenerator)
         {
+            var options = VsPackage.Instance.GetDialogPage(typeof(GeneralOptionPage)) as IGeneralOptions;
+            if (options?.InstallMissingPackages == false)
+            {
+                Trace.WriteLine("Skipping automatic depedency package installation");
+                return;
+            }
+
+            Trace.WriteLine("Checking required dependencies");
+
+            var version = await VS.Shell.GetVsVersionAsync();
+            if (version.Major >= 17)
+            {
+                await InstallMissingPackagesVS2022Async(project, codeGenerator).ConfigureAwait(false);
+            }
+            else
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                var dte = (DTE)await VsPackage.Instance.GetServiceAsync(typeof(DTE));
+                Assumes.Present(dte);
+                var dteProject = dte.GetActiveProject();
+                await InstallMissingPackagesAsync(dteProject, codeGenerator);
+            }
+        }
+
+        private static async Task InstallMissingPackagesVS2022Async(
+            VSProject project,
+            SupportedCodeGenerator codeGenerator)
+        {
+            var missingPackages = new List<PackageDependency>();
             var requiredPackages = codeGenerator.GetDependencies();
             foreach (var packageDependency in requiredPackages)
+            {
+                if (packageDependency.IsSystemLibrary)
+                {
+                    Trace.WriteLine("Package is a system library");
+                    if (!IsNetStandardLibrary(project))
+                    {
+                        Trace.WriteLine("Skipping package installation");
+                        continue;
+                    }
+                }
+
+                var packageId = packageDependency.Name;
+                var version = packageDependency.Version;
+
+                if (IsPackageInstalled(project, packageId) && !packageDependency.ForceUpdate)
+                {
+                    continue;
+                }
+
+                missingPackages.Add(packageDependency);
+            }
+
+            if (!missingPackages.Any())
+            {
+                return;
+            }
+
+            await project.UnloadAsync().ConfigureAwait(false);
+
+            foreach (var packageDependency in missingPackages)
             {
                 var packageId = packageDependency.Name;
                 var version = packageDependency.Version;
@@ -73,86 +142,109 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
                 await Task.Run(
                     () => process.Start(
                         "dotnet",
-                        $@"add ""{project.FullPath}"" package ""{packageId}"" --version {version}"));
+                        $@"add ""{project.FullPath}"" package ""{packageId}"" --version {version}"))
+
+                    .ConfigureAwait(false);
+            }
+
+            await Task.Delay(1000).ConfigureAwait(false);
+            await project.LoadAsync().ConfigureAwait(false);
+        }
+
+        public static DTEProject GetActiveProject(this DTE Dte)
+        {
+            try
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+
+                if (Dte.ActiveSolutionProjects is Array activeSolutionProjects && activeSolutionProjects.Length > 0)
+                    return activeSolutionProjects.GetValue(0) as DTEProject;
+
+                var doc = Dte.ActiveDocument;
+
+                if (doc != null && !string.IsNullOrEmpty(doc.FullName))
+                {
+                    var item = Dte.Solution?.FindProjectItem(doc.FullName);
+
+                    if (item != null)
+                        return item.ContainingProject;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.TrackError(ex);
+                Trace.WriteLine("Error getting the active project" + ex);
+            }
+
+            return null;
+        }
+
+        public static async Task InstallMissingPackagesAsync(
+            this DTEProject project,
+            SupportedCodeGenerator codeGenerator)
+        {
+            var componentModel = (IComponentModel)await VsPackage.Instance.GetServiceAsync(typeof(SComponentModel));
+            Assumes.Present(componentModel);
+
+            var packageInstaller = componentModel.GetService<IVsPackageInstaller>();
+            var installedServices = componentModel.GetService<IVsPackageInstallerServices>();
+            var installedPackages = installedServices.GetInstalledPackages(project)?.ToList() ?? new List<IVsPackageMetadata>();
+
+            var requiredPackages = codeGenerator.GetDependencies();
+            foreach (var packageDependency in requiredPackages)
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                InstallPackageDependency(project, packageDependency, installedPackages, packageInstaller);
             }
         }
 
-        //public static async Task InstallMissingPackagesAsync(
-        //    this Project project,
-        //    AsyncPackage package,
-        //    SupportedCodeGenerator codeGenerator)
-        //{
-        //    var options = VsPackage.Instance.GetDialogPage(typeof(GeneralOptionPage)) as IGeneralOptions;
-        //    if (options?.InstallMissingPackages == false)
-        //    {
-        //        Trace.WriteLine("Skipping automatic depedency package installation");
-        //        return;
-        //    }
+        private static void InstallPackageDependency(
+            DTEProject project,
+            PackageDependency packageDependency,
+            IReadOnlyCollection<IVsPackageMetadata> installedPackages,
+            IVsPackageInstaller packageInstaller)
+        {
+            var packageId = packageDependency.Name;
+            var version = packageDependency.Version;
 
-        //    Trace.WriteLine("Checking required dependencies");
+            if (installedPackages.Any(c => string.Equals(c.Id, packageId, StringComparison.InvariantCultureIgnoreCase)) &&
+               (installedPackages.Any(c => c.VersionString == version) || !packageDependency.ForceUpdate))
+            {
+                Trace.WriteLine($"{packageDependency.Name} is already installed");
+                return;
+            }
 
-        //    var componentModel = (IComponentModel)await package.GetServiceAsync(typeof(SComponentModel));
-        //    Assumes.Present(componentModel);
+            ThreadHelper.ThrowIfNotOnUIThread();
+            if (packageDependency.IsSystemLibrary)
+            {
+                Trace.WriteLine("Package is a system library");
+                if (!project.IsNetStandardLibrary())
+                {
+                    Trace.WriteLine("Skipping package installation");
+                    return;
+                }
+            }
 
-        //    var packageInstaller = componentModel.GetService<IVsPackageInstaller>();
-        //    var installedServices = componentModel.GetService<IVsPackageInstallerServices>();
-        //    var installedPackages = installedServices.GetInstalledPackages(project)?.ToList() ?? new List<IVsPackageMetadata>();
+            Trace.WriteLine($"Installing {packageId} version {version}");
 
-        //    var requiredPackages = codeGenerator.GetDependencies();
-        //    foreach (var packageDependency in requiredPackages)
-        //    {
-        //        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-        //        InstallPackageDependency(project, packageDependency, installedPackages, packageInstaller);
-        //    }
-        //}
+            try
+            {
+                packageInstaller.InstallPackage(
+                        null,
+                        project,
+                        packageId,
+                        version,
+                        true);
 
-        //private static void InstallPackageDependency(
-        //    Project project,
-        //    PackageDependency packageDependency,
-        //    IReadOnlyCollection<IVsPackageMetadata> installedPackages,
-        //    IVsPackageInstaller packageInstaller)
-        //{
-        //    var packageId = packageDependency.Name;
-        //    var version = packageDependency.Version;
-
-        //    if (installedPackages.Any(c => string.Equals(c.Id, packageId, StringComparison.InvariantCultureIgnoreCase)) &&
-        //       (installedPackages.Any(c => c.VersionString == version) || !packageDependency.ForceUpdate))
-        //    {
-        //        Trace.WriteLine($"{packageDependency.Name} is already installed");
-        //        return;
-        //    }
-
-        //    ThreadHelper.ThrowIfNotOnUIThread();
-        //    if (packageDependency.IsSystemLibrary)
-        //    {
-        //        Trace.WriteLine("Package is a system library");
-        //        if (!project.IsNetStandardLibrary())
-        //        {
-        //            Trace.WriteLine("Skipping package installation");
-        //            return;
-        //        }
-        //    }
-
-        //    Trace.WriteLine($"Installing {packageId} version {version}");
-
-        //    try
-        //    {
-        //        packageInstaller.InstallPackage(
-        //                null,
-        //                project,
-        //                packageId,
-        //                version,
-        //                true);
-
-        //        Trace.WriteLine($"Successfully installed {packageId} version {version}");
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logger.Instance.TrackError(e);
-        //        Trace.WriteLine($"Unable to install {packageId} version {version}");
-        //        Trace.WriteLine(e);
-        //    }
-        //}
+                Trace.WriteLine($"Successfully installed {packageId} version {version}");
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.TrackError(e);
+                Trace.WriteLine($"Unable to install {packageId} version {version}");
+                Trace.WriteLine(e);
+            }
+        }
 
         //public static string GetTopLevelNamespace(this Project item)
         //{
@@ -178,7 +270,7 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
         //    return null;
         //}
 
-        public static string GetTopLevelNamespace(this Community.VisualStudio.Toolkit.Project item)
+        public static string GetTopLevelNamespace(this VSProject item)
         {
             try
             {
@@ -197,27 +289,48 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Extensions
             return null;
         }
 
-        //public static bool IsNetStandardLibrary(this Project project)
-        //{
-        //    try
-        //    {
-        //        ThreadHelper.ThrowIfNotOnUIThread();
+        public static bool IsNetStandardLibrary(
+            this VSProject project)
+            => ProjectFileContainsString(project.FullPath, "<TargetFramework>netstandard");
 
-        //        var fileName = project.FileName;
-        //        Trace.WriteLine("Project filename = " + fileName);
-        //        var contents = File.ReadAllText(fileName);
-        //        var result = contents.Contains("<TargetFramework>netstandard");
-        //        return result;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logger.Instance.TrackError(e);
-        //        Trace.WriteLine("Unable to read project file contents");
-        //        Trace.WriteLine(e);
-        //    }
+        public static bool IsPackageInstalled(
+            this VSProject project,
+            string packageId)
+            => ProjectFileContainsString(project.FullPath, packageId);
 
-        //    return false;
-        //}
+        public static bool IsNetStandardLibrary(
+            this DTEProject project)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return ProjectFileContainsString(project.FullName, "<TargetFramework>netstandard");
+        }
+
+        public static bool IsPackageInstalled(
+            this DTEProject project,
+            string packageId)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return ProjectFileContainsString(project.FullName, packageId);
+        }
+
+        private static bool ProjectFileContainsString(string projectFilename, string text)
+        {
+            try
+            {
+                Trace.WriteLine("Project filename = " + projectFilename);
+                var contents = File.ReadAllText(projectFilename);
+                var result = contents.Contains(text.ToLowerInvariant());
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.TrackError(e);
+                Trace.WriteLine("Unable to read project file contents");
+                Trace.WriteLine(e);
+            }
+
+            return false;
+        }
 
         //public static async Task UpdatePropertyGroupsAsync(
         //    this Project project,
