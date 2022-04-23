9117a550ec519503773fb874c7648db8655d9fa8
Christian Helle <christian.helle@outlook.com>
2022-04-23 22:52:07 +0200

    Introduce OpenApiSupportedTargetFramework

---
+++ src/Core/ApiClientCodeGen.Core/Options/OpenApiGenerator/OpenApiSupportedTargetFramework.cs
@@ -0,0 +1,16 @@
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.OpenApiGenerator
+{
+    public enum OpenApiSupportedTargetFramework
+    {
+        NetStandard21,
+        NetStandard20,
+        NetStandard16,
+        NetStandard15,
+        NetStandard14,
+        NetStandard13,
+        NetCoreApp31,
+        Net47,
+        Net50,
+        Net60,
+    }
+}
