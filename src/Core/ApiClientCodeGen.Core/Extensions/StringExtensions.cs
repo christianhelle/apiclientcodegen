2c8b2b2eb394a96355bcb70bc25b12d4f7ef41ea
Christian Helle <crh@delegate.dk>
2022-04-24 11:18:29 +0200

    Fix target framework string values by using [Description] attribute on enum values

--- src/Core/ApiClientCodeGen.Core/Extensions/StringExtensions.cs
+++ src/Core/ApiClientCodeGen.Core/Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -60,6 +62,33 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions
                     stringBuilder.Append(b.ToString("x2"));
             }
             return stringBuilder.ToString();
+        }
+
+        public static string GetDescription<T>(this T e) where T : IConvertible
+        {
+            if (e is Enum)
+            {
+                Type type = e.GetType();
+                Array values = System.Enum.GetValues(type);
+
+                foreach (int val in values)
+                {
+                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memInfo = type.GetMember(type.GetEnumName(val));
+                        var descriptionAttribute = memInfo[0]
+                            .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                            .FirstOrDefault() as DescriptionAttribute;
+
+                        if (descriptionAttribute != null)
+                        {
+                            return descriptionAttribute.Description;
+                        }
+                    }
+                }
+            }
+
+            return null; // could also return string.Empty
         }
     }
 }
