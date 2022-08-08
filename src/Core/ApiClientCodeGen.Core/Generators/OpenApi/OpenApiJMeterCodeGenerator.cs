38ea995702d2ae9e46ed759cb73b9da8bcef229d
Christian Helle <christian.helle@outlook.com>
2022-08-08 20:56:17 +0200

    Implement ICodeGenerator for JMeter using OpenAPI Generator

---
+++ src/Core/ApiClientCodeGen.Core/Generators/OpenApi/OpenApiJMeterCodeGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi
+{
+    public class OpenApiJMeterCodeGenerator : ICodeGenerator
+    {
+        private readonly JavaPathProvider javaPathProvider;
+        private readonly IGeneralOptions options;
+        private readonly IProcessLauncher processLauncher;
+        private readonly IDependencyInstaller dependencyInstaller;
+        private readonly string swaggerFile;
+
+        public OpenApiJMeterCodeGenerator(
+            string swaggerFile,
+            IGeneralOptions options,
+            IProcessLauncher processLauncher,
+            IDependencyInstaller dependencyInstaller)
+        {
+            this.swaggerFile = swaggerFile;
+            this.options = options ?? throw new ArgumentNullException(nameof(options));
+            this.processLauncher = processLauncher ?? throw new ArgumentNullException(nameof(processLauncher));
+            this.dependencyInstaller = dependencyInstaller ?? throw new ArgumentNullException(nameof(dependencyInstaller));
+            javaPathProvider = new JavaPathProvider(options, processLauncher);
+        }
+
+        public string GenerateCode(IProgressReporter? pGenerateProgress)
+        {
+            try
+            {
+                pGenerateProgress?.Progress(10);
+
+                var jarFile = options.OpenApiGeneratorPath;
+                if (!File.Exists(jarFile))
+                {
+                    Trace.WriteLine(jarFile + " does not exist");
+                    jarFile = dependencyInstaller.InstallOpenApiGenerator();
+                }
+
+                pGenerateProgress?.Progress(30);
+
+                var output = Path.Combine(
+                    Path.GetDirectoryName(swaggerFile) ?? throw new InvalidOperationException(),
+                    "JMeter");
+
+                Directory.CreateDirectory(output);
+                pGenerateProgress?.Progress(40);
+
+                var arguments =
+                    $"-jar \"{jarFile}\" generate " +
+                    "--generator-name jmeter " +
+                    $"--input-spec \"{Path.GetFileName(swaggerFile)}\" " +
+                    $"--output \"{output}\" ";
+
+
+                processLauncher.Start(
+                    javaPathProvider.GetJavaExePath(),
+                    arguments,
+                    Path.GetDirectoryName(swaggerFile));
+
+                pGenerateProgress?.Progress(80);
+            }
+            finally
+            {
+                pGenerateProgress?.Progress(90);
+            }
+
+            return string.Empty;
+        }
+    }
+}
