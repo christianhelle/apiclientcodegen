2f25c610c49cb827e33165561d232f8a8f24d504
Christian Helle <christian.helle@outlook.com>
2022-08-09 22:44:31 +0200

    Add JMeter code generation integration tests

---
+++ src/Core/ApiClientCodeGen.Core.IntegrationTests/Generators/Yaml/OpenApiJMeterCodeGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ApiClientCodeGen.Tests.Common.Fixtures.Yaml;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ApiClientCodeGen.Core.IntegrationTests.Generators.Yaml
+{
+    [Trait("Category", "SkipWhenLiveUnitTesting")]
+    public class OpenApiJMeterCodeGeneratorTests : IClassFixture<OpenApiJMeterCodeGeneratorFixture>
+    {
+        private readonly OpenApiJMeterCodeGeneratorFixture fixture;
+
+        public OpenApiJMeterCodeGeneratorTests(OpenApiJMeterCodeGeneratorFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [SkippableFact(typeof(NotSupportedException))]
+        public void OpenApi_Reports_Progres()
+            => fixture.ProgressReporterMock.Verify(
+                c => c.Progress(It.IsAny<uint>(), It.IsAny<uint>()),
+                Times.AtLeastOnce);
+
+        [SkippableFact(typeof(NotSupportedException))]
+        public void Reads_JavaPath_From_Options()
+            => fixture.OptionsMock.Verify(c => c.JavaPath);
+    }
+}
