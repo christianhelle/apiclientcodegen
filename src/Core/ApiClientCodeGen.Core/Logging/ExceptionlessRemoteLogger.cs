53133d0d4dbd26afebb7d15f6b03cd538a562ef1
Christian Helle <christian.helle@outlook.com>
2022-08-12 00:02:08 +0200

    Disable exception logging due to Exceptionless rate limit

--- src/Core/ApiClientCodeGen.Core/Logging/ExceptionlessRemoteLogger.cs
+++ src/Core/ApiClientCodeGen.Core/Logging/ExceptionlessRemoteLogger.cs
@@ -61,9 +61,9 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
 
         public void TrackError(Exception exception)
         {
-            if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
-                return;
-            exception.ToExceptionless().Submit();
+            // if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
+            //     return;
+            // exception.ToExceptionless().Submit();
         }
 
         public void Disable()
@@ -76,11 +76,12 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
         {
             public void Run(EventPluginContext context)
             {
-                if (!context.ContextData.IsUnhandledError || !context.Event.IsError() || !context.ContextData.HasException())
-                    return;
-
-                var exception = context.ContextData.GetException();
-                context.Cancel = exception?.GetType()?.IsAssignableFrom(typeof(RapicgenException)) != true;
+                return;
+                // if (!context.ContextData.IsUnhandledError || !context.Event.IsError() || !context.ContextData.HasException())
+                //     return;
+                //
+                // var exception = context.ContextData.GetException();
+                // context.Cancel = exception?.GetType()?.IsAssignableFrom(typeof(RapicgenException)) != true;
             }
         }
     }
