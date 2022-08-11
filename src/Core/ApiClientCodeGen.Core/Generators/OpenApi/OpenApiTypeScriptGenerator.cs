1614f462c4119b841cc888c89b5b4f3e1696c40c
Christian Helle <christian.helle@outlook.com>
2022-08-12 00:45:51 +0200

    Fix build error due to lack of support for file scoped namespaces

--- src/Core/ApiClientCodeGen.Core/Generators/OpenApi/OpenApiTypeScriptGenerator.cs
+++ src/Core/ApiClientCodeGen.Core/Generators/OpenApi/OpenApiTypeScriptGenerator.cs
@@ -1,15 +1,16 @@
-﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi;
-
-public enum OpenApiTypeScriptGenerator
+﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi
 {
-    Angular,
-    Aurelia,
-    Axios,
-    Fetch,
-    Inversify,
-    JQuery,
-    NestJS,
-    Node,
-    ReduxQuery,
-    Rxjs,
+    public enum OpenApiTypeScriptGenerator
+    {
+        Angular,
+        Aurelia,
+        Axios,
+        Fetch,
+        Inversify,
+        JQuery,
+        NestJS,
+        Node,
+        ReduxQuery,
+        Rxjs,
+    }
 }
