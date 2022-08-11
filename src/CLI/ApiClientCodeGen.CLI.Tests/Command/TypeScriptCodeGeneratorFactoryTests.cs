1da5605082af8174d378d67d8b1348b082e45e55
Christian Helle <christian.helle@outlook.com>
2022-08-12 00:32:08 +0200

    Resolve warnings

--- src/CLI/ApiClientCodeGen.CLI.Tests/Command/TypeScriptCodeGeneratorFactoryTests.cs
+++ src/CLI/ApiClientCodeGen.CLI.Tests/Command/TypeScriptCodeGeneratorFactoryTests.cs
@@ -1,6 +1,7 @@
 using ApiClientCodeGen.Tests.Common.Infrastructure;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CLI.Commands;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
+using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
 using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
 using FluentAssertions;
@@ -8,17 +9,19 @@ using Xunit;
 
 namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CLI.Tests.Command
 {
-    public class JMeterCodeGeneratorFactoryTests
+    public class TypeScriptCodeGeneratorFactoryTests
     {
         [Theory, AutoMoqData]
         public void Create_Should_Return_NotNull(
-            JMeterCodeGeneratorFactory sut,
+            TypeScriptCodeGeneratorFactory sut,
+            OpenApiTypeScriptGenerator generator,
             string swaggerFile,
             string oututPath,
             IGeneralOptions options,
             IProcessLauncher processLauncher,
             IDependencyInstaller dependencyInstaller)
             => sut.Create(
+                    generator,
                     swaggerFile,
                     oututPath,
                     options,
