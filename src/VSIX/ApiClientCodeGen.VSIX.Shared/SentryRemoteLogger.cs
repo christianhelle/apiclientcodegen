ca060a5c64fc5b8311acae57a8024705e85bd740
Christian Helle <christian.helle@outlook.com>
2022-08-18 21:57:21 +0200

    Add tags and extra data to SentryRemoteLogger for VSIX

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/SentryRemoteLogger.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/SentryRemoteLogger.cs
@@ -15,12 +15,19 @@ namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient
             if (TestingUtility.IsRunningFromUnitTest || Debugger.IsAttached)
                 return;
             SentrySdk.Init("https://c925c0d0b9ea438a865377e066eeb8ff@o418075.ingest.sentry.io/5555548");
-            SentrySdk.ConfigureScope(scope => 
-                scope.User = new User
-                {
-                    Id = SupportInformation.GetAnonymousIdentity(),
-                    Username = SupportInformation.GetSupportKey()
-                });
+            SentrySdk.ConfigureScope(ConfigureScope);
+        }
+
+        private void ConfigureScope(Scope scope)
+        {
+            scope.SetTag("Platform", "VSIX");
+            scope.SetTag("SupportKey", SupportInformation.GetSupportKey());
+            scope.SetExtra("SupportKey", SupportInformation.GetSupportKey());
+            scope.User = new User
+            {
+                Id = SupportInformation.GetAnonymousIdentity(),
+                Username = SupportInformation.GetSupportKey()
+            };
         }
 
         public void TrackFeatureUsage(string featureName, params string[] tags)
