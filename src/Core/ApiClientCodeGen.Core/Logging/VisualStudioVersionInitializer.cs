4f2e7fe8b4382926f8ba03a495e82aab0e1016e7
Christian Helle <christian.helle@outlook.com>
2022-10-19 21:06:32 +0200

    Implement ITelemetryInitializer VisualStudioVersionInitializer

---
+++ src/Core/ApiClientCodeGen.Core/Logging/VisualStudioVersionInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Logging
+{
+    public sealed class VisualStudioVersionInitializer : ITelemetryInitializer
+    {
+        private readonly Version visualStudioVersion;
+
+        public VisualStudioVersionInitializer(Version visualStudioVersion)
+        {
+            this.visualStudioVersion = visualStudioVersion;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry is not ISupportProperties supportProperties)
+                return;
+            supportProperties.Properties["visual-studio-version"] = visualStudioVersion.ToString();
+        }
+    }
+}
