965caf24c4b96b4a077dc5ac4d5622e7a12f5e9b
Christian Helle <christian.helle@outlook.com>
2022-10-20 22:50:56 +0200

    Update RestSharp to v108.0.2

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/OpenApiGeneratorProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/OpenApiGeneratorProjectFileContents.cs
@@ -9,7 +9,7 @@
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.9.0"" />
-    <PackageReference Include = ""RestSharp"" Version=""108.0.1"" />
+    <PackageReference Include = ""RestSharp"" Version=""108.0.2"" />
     <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
@@ -23,7 +23,7 @@
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.9.0"" />
-    <PackageReference Include = ""RestSharp"" Version=""108.0.1"" />
+    <PackageReference Include = ""RestSharp"" Version=""108.0.2"" />
     <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
@@ -49,7 +49,7 @@
   </ItemGroup>
   <ItemGroup>
     <PackageReference Include = ""JsonSubTypes"" Version=""1.9.0"" />
-    <PackageReference Include = ""RestSharp"" Version=""108.0.1"" />
+    <PackageReference Include = ""RestSharp"" Version=""108.0.2"" />
     <PackageReference Include = ""Polly"" Version=""7.2.3"" />
     <PackageReference Include = ""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include = ""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
