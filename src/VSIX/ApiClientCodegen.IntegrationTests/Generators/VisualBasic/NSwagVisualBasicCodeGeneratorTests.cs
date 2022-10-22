1d1543b35dbcb0339c2433983b885754415bd13f
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:48 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodegen.IntegrationTests/Generators/VisualBasic/NSwagVisualBasicCodeGeneratorTests.cs

--- src/VSIX/ApiClientCodegen.IntegrationTests/Generators/VisualBasic/NSwagVisualBasicCodeGeneratorTests.cs
+++ src/VSIX/ApiClientCodegen.IntegrationTests/Generators/VisualBasic/NSwagVisualBasicCodeGeneratorTests.cs
@@ -1,14 +1,14 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Generators.NSwag;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Options;
+using Rapicgen.Generators.NSwag;
+using Rapicgen.Options;
 using FluentAssertions;
 using ICSharpCode.CodeConverter;
 using Microsoft.VisualStudio.Shell.Interop;
 
 using Moq;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.VisualBasic
+namespace Rapicgen.IntegrationTests.VisualBasic
 {
     
     [Xunit.Trait("Category", "SkipWhenLiveUnitTesting")]
