20e1388503fcc2d9c9840b97b57ed990dbada083
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:58 +0200

    Update namespace prefix to Rapicgen in src/VSMac/ApiClientCodeGen.VSMac/CustomTools/Swagger/SwaggerCodegenFactory.cs

--- src/VSMac/ApiClientCodeGen.VSMac/CustomTools/Swagger/SwaggerCodegenFactory.cs
+++ src/VSMac/ApiClientCodeGen.VSMac/CustomTools/Swagger/SwaggerCodegenFactory.cs
@@ -1,10 +1,10 @@
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.Swagger;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.NSwag;
+using Rapicgen.Core.Generators.OpenApi;
+using Rapicgen.Core.Generators.Swagger;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.General;
+using Rapicgen.Core.Options.NSwag;
 
 namespace ApiClientCodeGen.VSMac.CustomTools.Swagger
 {
