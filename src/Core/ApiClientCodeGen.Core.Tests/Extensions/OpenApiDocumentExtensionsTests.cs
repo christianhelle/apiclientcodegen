30561e0130b1cdb38ad468c69981fdffb52d52ce
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:42:42 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core.Tests/Extensions/OpenApiDocumentExtensionsTests.cs

--- src/Core/ApiClientCodeGen.Core.Tests/Extensions/OpenApiDocumentExtensionsTests.cs
+++ src/Core/ApiClientCodeGen.Core.Tests/Extensions/OpenApiDocumentExtensionsTests.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
 using ApiClientCodeGen.Tests.Common;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Extensions;
+using Rapicgen.Core.Extensions;
 using FluentAssertions;
 using NSwag;
 
