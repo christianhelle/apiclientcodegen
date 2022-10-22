e4b9bf0487723569acb25af3d7ed22a04e9f4984
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:19 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/OpenApi/OpenApiVisualBasicCodeGenerator.cs

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/OpenApi/OpenApiVisualBasicCodeGenerator.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/OpenApi/OpenApiVisualBasicCodeGenerator.cs
@@ -1,11 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Converters;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Rapicgen.Converters;
+using Rapicgen.Core;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.OpenApi
+namespace Rapicgen.CustomTool.OpenApi
 {
     [ExcludeFromCodeCoverage]
     [Guid("551706C0-F37F-4938-9DF8-9D3B2CBF452A")]
