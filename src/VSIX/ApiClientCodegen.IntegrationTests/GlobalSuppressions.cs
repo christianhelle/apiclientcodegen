deac72a58cd5eb9d6752540eb460ad6511936742
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:50 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodegen.IntegrationTests/GlobalSuppressions.cs

--- src/VSIX/ApiClientCodegen.IntegrationTests/GlobalSuppressions.cs
+++ src/VSIX/ApiClientCodegen.IntegrationTests/GlobalSuppressions.cs
@@ -6,9 +6,9 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "<Pending>", Scope = "member", Target = "~M:ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.NSwagStudioCodeGeneratorTests.NSwagStudio_Generate_Code_Using_NSwagStudio_From_SwaggerSpec~System.Threading.Tasks.Task")]
-[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.AutoRestCodeGeneratorTests.AutoRest_CSharp_Reports_Progres")]
-[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.OpenApiCodeGeneratorTests.OpenApi_Reports_Progres")]
-[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.NSwagCodeGeneratorTests.NSwag_Reports_Progres")]
-[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.SwaggerCodeGeneratorTests.Swagger_Reports_Progres")]
+[assembly: SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "<Pending>", Scope = "member", Target = "~M:Rapicgen.IntegrationTests.Generators.NSwagStudioCodeGeneratorTests.NSwagStudio_Generate_Code_Using_NSwagStudio_From_SwaggerSpec~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:Rapicgen.IntegrationTests.Generators.AutoRestCodeGeneratorTests.AutoRest_CSharp_Reports_Progres")]
+[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:Rapicgen.IntegrationTests.Generators.OpenApiCodeGeneratorTests.OpenApi_Reports_Progres")]
+[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:Rapicgen.IntegrationTests.Generators.NSwagCodeGeneratorTests.NSwag_Reports_Progres")]
+[assembly: SuppressMessage("Usage", "VSTHRD010:Use VS services from UI thread", Justification = "<Pending>", Scope = "member", Target = "~M:Rapicgen.IntegrationTests.Generators.SwaggerCodeGeneratorTests.Swagger_Reports_Progres")]
 
