1e2ddb0d11615ff1c6e5433e68a74f7f9fa3168b
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:42:37 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core.IntegrationTests/Generators/CSharp/OpenApiCodeGeneratorTests.cs

--- src/Core/ApiClientCodeGen.Core.IntegrationTests/Generators/CSharp/OpenApiCodeGeneratorTests.cs
+++ src/Core/ApiClientCodeGen.Core.IntegrationTests/Generators/CSharp/OpenApiCodeGeneratorTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using ApiClientCodeGen.Tests.Common.Build;
 using ApiClientCodeGen.Tests.Common.Fixtures;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Rapicgen.Core;
 using FluentAssertions;
 using Moq;
 using Xunit;
