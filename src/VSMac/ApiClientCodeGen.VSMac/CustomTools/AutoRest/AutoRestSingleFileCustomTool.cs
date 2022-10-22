6c3d32047e0330f9d39877d6232ff2249aadbda1
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:55 +0200

    Update namespace prefix to Rapicgen in src/VSMac/ApiClientCodeGen.VSMac/CustomTools/AutoRest/AutoRestSingleFileCustomTool.cs

--- src/VSMac/ApiClientCodeGen.VSMac/CustomTools/AutoRest/AutoRestSingleFileCustomTool.cs
+++ src/VSMac/ApiClientCodeGen.VSMac/CustomTools/AutoRest/AutoRestSingleFileCustomTool.cs
@@ -1,10 +1,10 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.AutoRest;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.NSwag;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.AutoRest;
 using MonoDevelop.Core;
 using MonoDevelop.Ide.CustomTools;
 using MonoDevelop.Projects;
