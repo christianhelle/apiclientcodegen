b799669beb39280db90395a29e5907e59597bc44
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:18 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/NSwag/NSwagVisualBasicCodeGenerator.cs

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/NSwag/NSwagVisualBasicCodeGenerator.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/NSwag/NSwagVisualBasicCodeGenerator.cs
@@ -1,11 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Converters;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Rapicgen.Converters;
+using Rapicgen.Core;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.NSwag
+namespace Rapicgen.CustomTool.NSwag
 {
     [ExcludeFromCodeCoverage]
     [Guid("5EB59F57-9C54-45F1-9301-59CBF65385D0")]
