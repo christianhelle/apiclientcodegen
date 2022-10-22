a697783e7d9b0e2e5dd6b5f9d8ca3cb8fee2d8e7
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:42:49 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core.Tests/Generators/Swagger/SwaggerCSharpCodeGeneratorExceptionTests.cs

--- src/Core/ApiClientCodeGen.Core.Tests/Generators/Swagger/SwaggerCSharpCodeGeneratorExceptionTests.cs
+++ src/Core/ApiClientCodeGen.Core.Tests/Generators/Swagger/SwaggerCSharpCodeGeneratorExceptionTests.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using ApiClientCodeGen.Tests.Common.Infrastructure;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.Swagger;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.Swagger;
+using Rapicgen.Core.Installer;
 using FluentAssertions;
 using Xunit;
 
