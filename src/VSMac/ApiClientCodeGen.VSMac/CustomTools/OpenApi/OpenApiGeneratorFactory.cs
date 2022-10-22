b9ac6de01a67c4e8451b72a8d6238911d6949b6c
Christian Helle <christian.helle@outlook.com>
2022-10-22 22:44:57 +0200

    Update namespace prefix to Rapicgen in src/VSMac/ApiClientCodeGen.VSMac/CustomTools/OpenApi/OpenApiGeneratorFactory.cs

--- src/VSMac/ApiClientCodeGen.VSMac/CustomTools/OpenApi/OpenApiGeneratorFactory.cs
+++ src/VSMac/ApiClientCodeGen.VSMac/CustomTools/OpenApi/OpenApiGeneratorFactory.cs
@@ -1,8 +1,8 @@
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.OpenApi;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.OpenApiGenerator;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.OpenApi;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.General;
+using Rapicgen.Core.Options.OpenApiGenerator;
 
 namespace ApiClientCodeGen.VSMac.CustomTools.OpenApi
 {
