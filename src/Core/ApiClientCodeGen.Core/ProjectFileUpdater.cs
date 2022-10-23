ad43adc2eb06ba56799ca19206493d2671316d12
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:35:34 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core/ProjectFileUpdater.cs

--- src/Core/ApiClientCodeGen.Core/ProjectFileUpdater.cs
+++ src/Core/ApiClientCodeGen.Core/ProjectFileUpdater.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core
+namespace Rapicgen.Core
 {
     public class ProjectFileUpdater
     {
