9db0523b85aeadf79142b294b93aab963e667ce6
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:36:16 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/AutoRest/AutoRestCSharpCodeGenerator.cs

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/AutoRest/AutoRestCSharpCodeGenerator.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/AutoRest/AutoRestCSharpCodeGenerator.cs
@@ -1,10 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Rapicgen.Core;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CustomTool.AutoRest
+namespace Rapicgen.CustomTool.AutoRest
 {
     [ExcludeFromCodeCoverage]
     [Guid("A2AE3194-DD0B-44FC-B8C4-B40EB2BF6498")]
