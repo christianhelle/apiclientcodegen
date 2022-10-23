da0ae67b67eaa55778942d876d0fd9e233b53d11
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:36:43 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodegen.IntegrationTests/Generators/CSharp/OpenApi3/Yaml/NSwagStudioCodeGeneratorTests.cs

--- src/VSIX/ApiClientCodegen.IntegrationTests/Generators/CSharp/OpenApi3/Yaml/NSwagStudioCodeGeneratorTests.cs
+++ src/VSIX/ApiClientCodegen.IntegrationTests/Generators/CSharp/OpenApi3/Yaml/NSwagStudioCodeGeneratorTests.cs
@@ -1,10 +1,10 @@
 ﻿using ApiClientCodeGen.Tests.Common.Build;
 using ApiClientCodeGen.Tests.Common.Fixtures.OpenApi3.Yaml;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core;
+using Rapicgen.Core;
 using FluentAssertions;
 using Xunit;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.IntegrationTests.Generators.CSharp.OpenApi3.Yaml
+namespace Rapicgen.IntegrationTests.Generators.CSharp.OpenApi3.Yaml
 {
     [Trait("Category", "SkipWhenLiveUnitTesting")]
     public class NSwagStudioCodeGeneratorTests : IClassFixture<NSwagStudioCodeGeneratorFixture>
