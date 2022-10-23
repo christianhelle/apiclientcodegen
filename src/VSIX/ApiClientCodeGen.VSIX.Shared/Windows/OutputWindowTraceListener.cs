94677098f1a683397a18a063d01c6ce05132932e
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:36:35 +0200

    Update namespace prefix to Rapicgen in src/VSIX/ApiClientCodeGen.VSIX.Shared/Windows/OutputWindowTraceListener.cs

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/Windows/OutputWindowTraceListener.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/Windows/OutputWindowTraceListener.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Windows
+namespace Rapicgen.Windows
 {
     [ExcludeFromCodeCoverage]
     public class OutputWindowTraceListener : TraceListener
