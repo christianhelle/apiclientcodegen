3b9d2479761bb7bb21f8e7c909f2002acd7de981
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:34:47 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core.Tests/Generators/NSwagStudio/NSwagStudioFileHelperTests.cs

--- src/Core/ApiClientCodeGen.Core.Tests/Generators/NSwagStudio/NSwagStudioFileHelperTests.cs
+++ src/Core/ApiClientCodeGen.Core.Tests/Generators/NSwagStudio/NSwagStudioFileHelperTests.cs
@@ -1,8 +1,8 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using ApiClientCodeGen.Tests.Common;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwagStudio;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwagStudio;
+using Rapicgen.Core.Generators.NSwagStudio;
+using Rapicgen.Core.Options.NSwagStudio;
 using Moq;
 
 namespace ApiClientCodeGen.Core.Tests.Generators.NSwagStudio
