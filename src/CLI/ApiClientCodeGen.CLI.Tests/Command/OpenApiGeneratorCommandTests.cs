6e506b0805f139d987a8690e3c137c3e99da5098
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:34:18 +0200

    Update namespace prefix to Rapicgen in src/CLI/ApiClientCodeGen.CLI.Tests/Command/OpenApiGeneratorCommandTests.cs

--- src/CLI/ApiClientCodeGen.CLI.Tests/Command/OpenApiGeneratorCommandTests.cs
+++ src/CLI/ApiClientCodeGen.CLI.Tests/Command/OpenApiGeneratorCommandTests.cs
@@ -2,15 +2,15 @@ using System;
 using System.IO;
 using ApiClientCodeGen.Tests.Common.Infrastructure;
 using AutoFixture.Xunit2;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CLI.Commands;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Installer;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.General;
+using Rapicgen.CLI.Commands;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.General;
 using FluentAssertions;
 using Moq;
 using Xunit;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.CLI.Tests.Command
+namespace Rapicgen.CLI.Tests.Command
 {
     public class OpenApiGeneratorCommandTests
     {
