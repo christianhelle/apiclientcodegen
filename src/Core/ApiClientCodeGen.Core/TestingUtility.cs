cd0c925ab1f67f8bdc2e4fbc637ddb8ad388cf58
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:35:36 +0200

    Update namespace prefix to Rapicgen in src/Core/ApiClientCodeGen.Core/TestingUtility.cs

--- src/Core/ApiClientCodeGen.Core/TestingUtility.cs
+++ src/Core/ApiClientCodeGen.Core/TestingUtility.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 
-namespace ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core
+namespace Rapicgen.Core
 {
     [ExcludeFromCodeCoverage]
     public static class TestingUtility
