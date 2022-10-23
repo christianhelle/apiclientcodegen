17b3fe582ed1ba3bd34348402c34257bbcecf4e1
Christian Helle <christian.helle@outlook.com>
2022-10-23 09:36:55 +0200

    Update namespace prefix to Rapicgen in src/VSMac/ApiClientCodeGen.VSMac/CustomTools/NSwag/NSwagCodeGeneratorFactory.cs

--- src/VSMac/ApiClientCodeGen.VSMac/CustomTools/NSwag/NSwagCodeGeneratorFactory.cs
+++ src/VSMac/ApiClientCodeGen.VSMac/CustomTools/NSwag/NSwagCodeGeneratorFactory.cs
@@ -1,6 +1,6 @@
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Generators.NSwag;
-using ChristianHelle.DeveloperTools.CodeGenerators.ApiClient.Core.Options.NSwag;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.NSwag;
+using Rapicgen.Core.Options.NSwag;
 
 namespace ApiClientCodeGen.VSMac.CustomTools.NSwag
 {
