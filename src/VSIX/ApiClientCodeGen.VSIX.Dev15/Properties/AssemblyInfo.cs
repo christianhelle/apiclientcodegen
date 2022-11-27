5a3fa140296689c72c7d5cf23ab3e39fa5699cd7
Christian Helle <christian.helle@outlook.com>
2022-11-27 12:00:32 +0100

    Introduce VSIX project for Visual Studio 2017

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Dev15/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("REST API Client Code Generator")]
+[assembly: AssemblyDescription("A collection of Visual Studio custom tool code generators for Swagger / OpenAPI specification files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("REST API Client Code Generator")]
+[assembly: AssemblyCopyright("© 2019 Christian Resma Helle All Rights Reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
