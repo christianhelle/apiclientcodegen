9ba537d9ff77031c2f2975e8e50d38f98ecf47dd
Christian Helle <christian.helle@outlook.com>
2023-01-28 13:27:37 +0100

    Introduce initial KiotaCodeGenerator

---
+++ src/Core/ApiClientCodeGen.Core/Generators/Kiota/KiotaCodeGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Rapicgen.Core.Installer;
+using Rapicgen.Core.Options.General;
+
+namespace Rapicgen.Core.Generators.Kiota;
+
+public class KiotaCodeGenerator : ICodeGenerator
+{
+    private readonly string swaggerFile;
+    private readonly string outputPath;
+    private readonly IProcessLauncher processLauncher;
+    private readonly IDependencyInstaller dependencyInstaller;
+
+    public KiotaCodeGenerator(
+        string swaggerFile,
+        string outputPath,
+        IProcessLauncher processLauncher,
+        IDependencyInstaller dependencyInstaller)
+    {
+        this.swaggerFile = swaggerFile;
+        this.outputPath = outputPath;
+        this.processLauncher = processLauncher;
+        this.dependencyInstaller = dependencyInstaller;
+    }
+    
+    public string GenerateCode(IProgressReporter? pGenerateProgress)
+    {
+        try
+        {
+            pGenerateProgress?.Progress(10);
+
+            dependencyInstaller.InstallKiota();
+            
+            pGenerateProgress?.Progress(30);
+            
+            processLauncher.Start(
+                PathProvider.GetDotNetPath(),
+                $"kiota generate -l CSharp -d {swaggerFile} -o {outputPath}");
+            
+            pGenerateProgress?.Progress(80);
+            
+            return CSharpFileMerger.MergeFilesAndDeleteSource(outputPath);
+        }
+        finally
+        {
+            pGenerateProgress?.Progress(100);
+        }
+    }
+}
