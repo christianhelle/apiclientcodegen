ebd54977c1c8cd03fe21e4d269591a6036240f43
Christian Helle <christian.helle@outlook.com>
2023-01-29 15:04:03 +0100

    Remove try/finally in KiotaCodeGenerator

--- src/Core/ApiClientCodeGen.Core/Generators/Kiota/KiotaCodeGenerator.cs
+++ src/Core/ApiClientCodeGen.Core/Generators/Kiota/KiotaCodeGenerator.cs
@@ -23,27 +23,23 @@ public class KiotaCodeGenerator : ICodeGenerator
         this.processLauncher = processLauncher;
         this.dependencyInstaller = dependencyInstaller;
     }
-    
+
     public string GenerateCode(IProgressReporter? pGenerateProgress)
     {
-        try
-        {
-            pGenerateProgress?.Progress(10);
-            dependencyInstaller.InstallKiota();
-            
-            pGenerateProgress?.Progress(30);
-            var outputFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-            Directory.CreateDirectory(outputFolder);
-            
-            pGenerateProgress?.Progress(40);
-            processLauncher.Start("kiota", $" generate -l CSharp -d {swaggerFile} -o {outputFolder}");
-            
-            pGenerateProgress?.Progress(80);
-            return CSharpFileMerger.MergeFiles(outputFolder);
-        }
-        finally
-        {
-            pGenerateProgress?.Progress(100);
-        }
+        pGenerateProgress?.Progress(10);
+        dependencyInstaller.InstallKiota();
+
+        pGenerateProgress?.Progress(30);
+        var outputFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(outputFolder);
+
+        pGenerateProgress?.Progress(40);
+        processLauncher.Start("kiota", $" generate -l CSharp -d {swaggerFile} -o {outputFolder}");
+
+        pGenerateProgress?.Progress(80);
+        var output = CSharpFileMerger.MergeFiles(outputFolder);
+
+        pGenerateProgress?.Progress(100);
+        return output;
     }
 }
