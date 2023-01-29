f42f052943e6ae82d5a6cdecdebca5a2a3833bed
Christian Helle <christian.helle@outlook.com>
2023-01-29 23:19:08 +0100

    Add KiotaCommand tests

---
+++ src/CLI/ApiClientCodeGen.CLI.Tests/Command/KiotaCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using ApiClientCodeGen.Tests.Common.Infrastructure;
+using FluentAssertions;
+using McMaster.Extensions.CommandLineUtils;
+using Rapicgen.CLI.Commands.CSharp;
+using Rapicgen.Core.Generators.Kiota;
+using Xunit;
+
+namespace Rapicgen.CLI.Tests.Command;
+
+public class KiotaCommandTests
+{
+    [Theory, AutoMoqData]
+    public void CreateGenerator_WhenCalled_ReturnsKiotaCodeGenerator(
+        KiotaCommand sut)
+    {
+        var result = sut.CreateGenerator();
+        Assert.IsType<KiotaCodeGenerator>(result);
+    }
+    
+    [Fact]
+    public void HasAttribute_Command()
+    {
+        typeof(KiotaCommand)
+            .GetCustomAttributes(false)
+            .Should()
+            .Contain(x => x is CommandAttribute);
+    }
+    
+    [Fact]
+    public void CommandAttribute_Name_Is_Kiota()
+    {
+        typeof(KiotaCommand)
+            .GetCustomAttributes(false)
+            .OfType<CommandAttribute>()
+            .First()
+            .Name
+            .Should()
+            .Be("kiota");
+    }
+}
