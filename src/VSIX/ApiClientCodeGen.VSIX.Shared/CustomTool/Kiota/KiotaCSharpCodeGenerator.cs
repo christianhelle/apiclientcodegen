91d26cbfc7a42bd8a6159c075995df155f3d01c1
Christian Helle <christian.helle@outlook.com>
2023-01-31 23:00:55 +0100

    Introduce KiotaCodeGenerator custom tool for Visual Studio

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/Kiota/KiotaCSharpCodeGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Rapicgen.Core;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace Rapicgen.CustomTool.Kiota
+{
+    [ExcludeFromCodeCoverage]
+    [Guid("06374E1D-C94A-431B-938D-50A9A7C1D362")]
+    [ComVisible(true)]
+    [ProvideObject(typeof(KiotaCSharpCodeGenerator))]
+    [CodeGeneratorRegistration(typeof(KiotaCSharpCodeGenerator),
+                              Description,
+                              ProvideCodeGeneratorAttribute.CSharpProjectGuid,
+                              GeneratesDesignTimeSource = true,
+                              GeneratorRegKeyName = "KiotaCodeGenerator")]
+    public class KiotaCSharpCodeGenerator : KiotaCodeGenerator
+    {
+        public const string Description = "C# Kiota Client Code Generator";
+
+        public KiotaCSharpCodeGenerator()
+            : base(SupportedLanguage.CSharp)
+        {
+        }
+
+        public override int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".cs";
+            return 0;
+        }
+    }
+}
---
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/Kiota/KiotaCodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Rapicgen.Core;
+using Rapicgen.Core.Converters;
+
+namespace Rapicgen.CustomTool.Kiota
+{
+    [ExcludeFromCodeCoverage]
+    [ComVisible(true)]
+    public abstract class KiotaCodeGenerator : SingleFileCodeGenerator
+    {
+        protected KiotaCodeGenerator(
+            SupportedLanguage language,
+            ILanguageConverter? languageConverter = null)
+            : base(SupportedCodeGenerator.Kiota, language)
+        {
+        }
+    }
+}
