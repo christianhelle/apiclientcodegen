acbb02b7f11dbbae62348b2165233b1e7141cf22
Christian Helle <christian.helle@outlook.com>
2023-02-25 23:23:35 +0100

    Introduce RefitCodeGenerator implementation of ICodeGenerator

---
+++ src/Core/ApiClientCodeGen.Core/Generators/Refit/RefitCodeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Refitter.Core;
+
+namespace Rapicgen.Core.Generators.Refit;
+
+public class RefitCodeGenerator : ICodeGenerator
+{
+    private readonly string swaggerFile;
+    private readonly string defaultNamespace;
+
+    public RefitCodeGenerator(
+        string swaggerFile,
+        string defaultNamespace)
+    {
+        this.swaggerFile = swaggerFile;
+        this.defaultNamespace = defaultNamespace;
+    }
+    
+    public string GenerateCode(IProgressReporter? pGenerateProgress)
+    {
+        pGenerateProgress?.Progress(10);
+        var generator = RefitGenerator.Create(
+            new RefitGeneratorSettings
+            {
+                OpenApiPath = swaggerFile,
+                Namespace = defaultNamespace
+            })
+            .GetAwaiter()
+            .GetResult();
+        
+        pGenerateProgress?.Progress(50);
+        var code = generator.Generate();
+        
+        pGenerateProgress?.Progress(90);
+        return code;
+    }
+}
