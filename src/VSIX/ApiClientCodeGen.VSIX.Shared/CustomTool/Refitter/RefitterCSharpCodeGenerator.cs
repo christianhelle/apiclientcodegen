83d32fd743d076e3a56556eb24bafa5d15275179
Christian Helle <christian.helle@outlook.com>
2023-02-26 15:53:37 +0100

    Imlpement SingleFileCodeGenerator for Refitter

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/Refitter/RefitterCSharpCodeGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Rapicgen.Core;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace Rapicgen.CustomTool.Refitter
+{
+    [ExcludeFromCodeCoverage]
+    [Guid("8CED7707-DCAE-4304-AEAF-27E2508902F0")]
+    [ComVisible(true)]
+    [ProvideObject(typeof(RefitterCSharpCodeGenerator))]
+    [CodeGeneratorRegistration(typeof(RefitterCSharpCodeGenerator),
+                              Description,
+                              ProvideCodeGeneratorAttribute.CSharpProjectGuid,
+                              GeneratesDesignTimeSource = true,
+                              GeneratorRegKeyName = "RefitterCodeGenerator")]
+    public class RefitterCSharpCodeGenerator : RefitterCodeGenerator
+    {
+        public const string Description = "C# Refit Client Code Generator";
+
+        public RefitterCSharpCodeGenerator()
+            : base(SupportedLanguage.CSharp)
+        {
+        }
+
+        public override int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".cs";
+            return 0;
+        }
+    }
+}
---
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/CustomTool/Refitter/RefitterCodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Rapicgen.Core;
+using Rapicgen.Core.Converters;
+
+namespace Rapicgen.CustomTool.Refitter
+{
+    [ExcludeFromCodeCoverage]
+    [ComVisible(true)]
+    public abstract class RefitterCodeGenerator : SingleFileCodeGenerator
+    {
+        protected RefitterCodeGenerator(
+            SupportedLanguage language,
+            ILanguageConverter? languageConverter = null)
+            : base(SupportedCodeGenerator.Refitter, language)
+        {
+        }
+    }
+}
