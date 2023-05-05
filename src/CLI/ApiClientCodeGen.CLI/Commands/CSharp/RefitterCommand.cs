9cc7c4cf69b9390b4179b36a80e798b50adea79b
Christian Helle <christian.helle@outlook.com>
2023-05-05 15:43:45 +0200

    Upgrade Refitter version in CLI tool to v0.5.3

--- src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
+++ src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
@@ -6,7 +6,7 @@ using Rapicgen.Core.Options.Refitter;
 
 namespace Rapicgen.CLI.Commands.CSharp;
 
-[Command("refitter", Description = "Refitter (v0.5.2)")]
+[Command("refitter", Description = "Refitter (v0.5.3)")]
 public class RefitterCommand : CodeGeneratorCommand
 {
     private readonly IRefitterCodeGeneratorFactory factory;
