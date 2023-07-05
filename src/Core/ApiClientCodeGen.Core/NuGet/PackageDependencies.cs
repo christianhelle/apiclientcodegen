a330a5dd2de5c9bea7894b909e3875b510cd4723
Christian Helle <christian.helle@outlook.com>
2023-07-05 10:57:23 +0200

    Update Microsoft.Kiota.Abstractions in package dependency installer to v1.2.1

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -76,7 +76,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency MicrosoftKiotaAbstractions =
             new PackageDependency(
                 "Microsoft.Kiota.Abstractions",
-                new Version(1, 2, 0));
+                "1.2.1");
 
         public static readonly PackageDependency MicrosoftKiotaAuthenticationAzure =
             new PackageDependency(
