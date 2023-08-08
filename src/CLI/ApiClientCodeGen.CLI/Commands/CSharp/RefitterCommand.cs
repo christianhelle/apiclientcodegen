a598b32fad08f9913cac28363a241a4e1eed64b9
Christian Helle <christian.helle@outlook.com>
2023-08-08 08:58:45 +0200

    Update Refitter version in <auto-generated> headers to v0.7.2
    The command description for Refitter in the CLI was reflecting an outdated version (v0.7.1). This commit updates the version to the correct one (v0.7.2) as reflected in the RefitterCommand.cs file, ensuring consistency in versioning across the application.

--- src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
+++ src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
@@ -6,7 +6,7 @@ using Rapicgen.Core.Options.Refitter;
 
 namespace Rapicgen.CLI.Commands.CSharp;
 
-[Command("refitter", Description = "Refitter (v0.7.1)")]
+[Command("refitter", Description = "Refitter (v0.7.2)")]
 public class RefitterCommand : CodeGeneratorCommand
 {
     private readonly IRefitterCodeGeneratorFactory factory;
