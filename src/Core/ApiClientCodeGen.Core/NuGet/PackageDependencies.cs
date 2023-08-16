73fde5f00ea8ce4f682a4bdad0465907ee82ed40
Christian Helle <christian.helle@outlook.com>
2023-08-16 12:19:06 +0200

    Update version format in NuGet package dependencies
    Changed the format of versions of NuGet packages in PackageDependencies.cs from Version object to string for easier maintenance and readability. Now, the version of each package is just a string like "1.8.0" instead of new Version(1, 8, 0). This will simplify future updates and comparisons.

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -7,50 +7,50 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency NewtonsoftJson =
             new PackageDependency(
                 "Newtonsoft.Json",
-                new Version(13, 0, 3, 0),
+                "13.0.3",
                 false);
 
         public static readonly PackageDependency MicrosoftRestClientRuntime =
             new PackageDependency(
                 "Microsoft.Rest.ClientRuntime",
-                new Version(2, 3, 24, 0));
+                "2.3.24");
 
         public static readonly PackageDependency RestSharp =
             new PackageDependency(
                 "RestSharp",
-                new Version(105, 1, 0, 0));
+                "105.1.0");
 
         public static readonly PackageDependency JsonSubTypes =
             new PackageDependency(
                 "JsonSubTypes",
-                new Version(1, 8, 0, 0));
+                "1.8.0");
 
         public static readonly PackageDependency RestSharpLatest =
             new PackageDependency(
                 "RestSharp",
-                new Version(108, 0, 2, 0));
+                "108.0.2");
 
         public static readonly PackageDependency JsonSubTypesLatest =
             new PackageDependency(
                 "JsonSubTypes",
-                new Version(2, 0, 1, 0));
+                "2.0.1");
 
         public static readonly PackageDependency SystemRuntimeSerializationPrimitives =
             new PackageDependency(
                 "System.Runtime.Serialization.Primitives",
-                new Version(4, 3, 0),
+                "4.3.0",
                 isSystemLibrary: true);
 
         public static readonly PackageDependency SystemComponentModelAnnotations =
             new PackageDependency(
                 "System.ComponentModel.Annotations",
-                new Version(5, 0, 0),
+                "5.0.0",
                 isSystemLibrary: true);
 
         public static readonly PackageDependency MicrosoftCSharp =
             new PackageDependency(
                 "Microsoft.CSharp",
-                new Version(4, 5, 0),
+                "4.5.0",
                 isSystemLibrary: true);
 
         public static readonly PackageDependency Polly =
@@ -66,7 +66,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency AzureCore =
             new PackageDependency(
                 "Azure.Core",
-                new Version(1, 34, 0));
+                "1.34.0");
 
         public static readonly PackageDependency AzureIdentity =
             new PackageDependency(
@@ -111,6 +111,6 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency Refit =
             new PackageDependency(
                 "Refit",
-                new Version(7, 0, 0));
+                "7.0.0");
     }
 }
