49ecaef9f7459e0de901936b328fbc2becc43815
Christian Helle <christian.helle@outlook.com>
2023-09-08 10:09:01 +0200

    Bump up Azure.Core to v1.35.0

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -66,7 +66,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency AzureCore =
             new PackageDependency(
                 "Azure.Core",
-                "1.34.0");
+                "1.35.0");
 
         public static readonly PackageDependency AzureIdentity =
             new PackageDependency(
