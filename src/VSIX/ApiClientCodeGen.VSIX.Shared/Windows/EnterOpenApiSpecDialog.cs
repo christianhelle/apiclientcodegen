5223a70f22f138aa0249ba8cc4eb5da48e9bd092
Christian Helle <christian.helle@outlook.com>
2023-11-01 21:54:01 +0100

    Update error message in OpenApiSpecDialog
    Adjusted the error display in 'EnterOpenApiSpecDialog.cs' to include exception type. The old version only showed the exception message, the new version adds the type of exception thrown in front of the message. This ensures more helpful diagnostic information for debugging.

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/Windows/EnterOpenApiSpecDialog.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/Windows/EnterOpenApiSpecDialog.cs
@@ -106,7 +106,7 @@ namespace Rapicgen.Windows
             }
             catch (Exception ex)
             {
-                lblStatus.Text = ex.Message;
+                lblStatus.Text = ex.GetType().Name + ": " + ex.Message;
                 Logger.Instance.TrackError(ex);
                 Logger.Instance.WriteLine($"Unable to download OpenAPI specification file from {url}");
                 Logger.Instance.WriteLine(ex);
