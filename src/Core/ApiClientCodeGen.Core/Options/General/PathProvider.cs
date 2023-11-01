07bee57ae27d6785377c27cc30f1e5369ef53a2f
Christian Helle <christian.helle@outlook.com>
2023-11-01 21:41:05 +0100

    Updated error handling in PathProvider.cs
    Enhanced error handling logic in the PathProvider class to improve code robustness. If the JAVA_HOME environment variable is not set or empty, the code will output a log message and return the string "java", instead of throwing an exception. This avoids potential application crashes.

--- src/Core/ApiClientCodeGen.Core/Options/General/PathProvider.cs
+++ src/Core/ApiClientCodeGen.Core/Options/General/PathProvider.cs
@@ -11,12 +11,16 @@ namespace Rapicgen.Core.Options.General
             try
             {
                 var javaHome = Environment.GetEnvironmentVariable(environmentVariable);
-                var javaExe = Path.Combine(javaHome ?? throw new InvalidOperationException(), "bin\\java.exe");
-                return javaExe;
+                if (!string.IsNullOrWhiteSpace(javaHome)) 
+                    return Path.Combine(javaHome, "bin\\java.exe");
+
+                Logger.Instance.WriteLine("Unable to read JAVA_HOME environment variable");
+                return "java";
+
             }
             catch (Exception e)
             {
-                Logger.Instance.WriteLine("Unable to find JAVA_HOME environment variable");
+                Logger.Instance.TrackError(e);
                 return "java";
             }
         }
