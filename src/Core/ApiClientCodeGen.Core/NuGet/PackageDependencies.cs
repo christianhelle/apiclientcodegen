a8d6e8233991885820fbf6f84767bac565d7852a
Christian Helle <christian.helle@outlook.com>
2024-02-26 14:02:11 +0100

    Update Microsoft.Kiota.Abstractions package version to 1.7.10

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -76,7 +76,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency MicrosoftKiotaAbstractions =
             new PackageDependency(
                 "Microsoft.Kiota.Abstractions",
-                "1.7.9");
+                "1.7.10");
 
         public static readonly PackageDependency MicrosoftKiotaAuthenticationAzure =
             new PackageDependency(
