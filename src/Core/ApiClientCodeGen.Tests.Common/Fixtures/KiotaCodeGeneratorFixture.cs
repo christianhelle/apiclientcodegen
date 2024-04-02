d2dbe2c782d298aac1a5504dcd2c0f48a361ce53
Christian Helle <christian.helle@outlook.com>
2024-04-02 22:56:52 +0200

    Add integration tests for Kiota code generation

---
+++ src/Core/ApiClientCodeGen.Tests.Common/Fixtures/KiotaCodeGeneratorFixture.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Moq;
+using Rapicgen.Core;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.Kiota;
+using Rapicgen.Core.Installer;
+using Xunit;
+
+namespace ApiClientCodeGen.Tests.Common.Fixtures
+{
+    [Trait("Category", "SkipWhenLiveUnitTesting")]
+    public class KiotaCodeGeneratorFixture : TestWithResources
+    {
+        public readonly Mock<IProgressReporter> ProgressReporterMock = new Mock<IProgressReporter>();
+
+        public string Code { get; private set; }
+
+        protected override async Task OnInitializeAsync()
+        {
+            const string defaultNamespace = "GeneratedCode";
+            var codeGenerator = new KiotaCodeGenerator(
+                Path.GetFullPath(SwaggerJsonFilename),
+                defaultNamespace,
+                new ProcessLauncher(),
+                new DependencyInstaller(
+                    new NpmInstaller(new ProcessLauncher()),
+                    new FileDownloader(new WebDownloader()),
+                    new ProcessLauncher()));
+
+            Code = codeGenerator.GenerateCode(ProgressReporterMock.Object);
+        }
+    }
+}
