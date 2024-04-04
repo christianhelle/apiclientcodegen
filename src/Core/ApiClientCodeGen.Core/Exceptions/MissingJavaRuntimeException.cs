a28f83750289a80c88065991864864167df5fcd7
Christian Helle <christian.helle@outlook.com>
2024-04-04 11:18:01 +0200

    Add MissingJavaRuntimeException class

---
+++ src/Core/ApiClientCodeGen.Core/Exceptions/MissingJavaRuntimeException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class MissingJavaRuntimeException : Exception
+{
+    public MissingJavaRuntimeException() { }
+    public MissingJavaRuntimeException(string message) : base(message) { }
+    public MissingJavaRuntimeException(string message, Exception inner) : base(message, inner) { }
+}
