6bfa0b8d84762de6a4b79578b5a86ef90e1b9dce
Christian Helle <christian.helle@outlook.com>
2024-04-10 13:57:20 +0200

    Update Azure.Identity package version to 1.11.0

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -71,7 +71,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency AzureIdentity =
             new PackageDependency(
                 "Azure.Identity",
-                "1.10.4");
+                "1.11.0");
 
         public static readonly PackageDependency MicrosoftKiotaAbstractions =
             new PackageDependency(
