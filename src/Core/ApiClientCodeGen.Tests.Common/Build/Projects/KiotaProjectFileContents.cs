58d01f36056ec39fe764fe579a9482cf9fc96eea
Christian Helle <christian.helle@outlook.com>
2024-04-17 15:12:22 +0200

    Bump Azure.Identity version from 1.11.0 to 1.11.1

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/KiotaProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/KiotaProjectFileContents.cs
@@ -9,7 +9,7 @@
     <TargetFramework>net6.0</TargetFramework>
   </PropertyGroup>
   <ItemGroup>
-    <PackageReference Include=""Azure.Identity"" Version=""1.11.0"" />
+    <PackageReference Include=""Azure.Identity"" Version=""1.11.1"" />
     <PackageReference Include=""Microsoft.Kiota.Abstractions"" Version=""1.8.0"" />
     <PackageReference Include=""Microsoft.Kiota.Authentication.Azure"" Version=""1.1.4"" />
     <PackageReference Include=""Microsoft.Kiota.Http.HttpClientLibrary"" Version=""1.3.8"" />
@@ -27,7 +27,7 @@
     <TargetFramework>netstandard2.1</TargetFramework>
   </PropertyGroup>
   <ItemGroup>
-    <PackageReference Include=""Azure.Identity"" Version=""1.11.0"" />
+    <PackageReference Include=""Azure.Identity"" Version=""1.11.1"" />
     <PackageReference Include=""Microsoft.Kiota.Abstractions"" Version=""1.8.0"" />
     <PackageReference Include=""Microsoft.Kiota.Authentication.Azure"" Version=""1.1.4"" />
     <PackageReference Include=""Microsoft.Kiota.Http.HttpClientLibrary"" Version=""1.3.8"" />
