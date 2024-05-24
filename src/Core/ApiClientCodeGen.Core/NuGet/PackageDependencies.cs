2971e9d8a049a8afc083165f70c9e1cfb2a044c7
Christian Helle <christian.helle@outlook.com>
2024-05-24 10:56:15 +0200

    Bump Microsoft.Kiota.Serialization.Json to version 1.3.2 in PackageDependencies

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -96,7 +96,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency MicrosoftKiotaSerializationJson =
             new PackageDependency(
                 "Microsoft.Kiota.Serialization.Json",
-                "1.3.1");
+                "1.3.2");
 
         public static readonly PackageDependency MicrosoftKiotaSerializationText =
             new PackageDependency(
