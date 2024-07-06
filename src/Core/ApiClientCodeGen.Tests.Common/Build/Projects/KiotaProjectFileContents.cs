b4e08cc0bcdd2170a803d29cbd4d73481540b3ca
Christian Helle <christian.helle@outlook.com>
2024-07-06 15:53:31 +0200

    Update Microsoft Kiota package versions to latest in KiotaProjectFileContents.cs

--- src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/KiotaProjectFileContents.cs
+++ src/Core/ApiClientCodeGen.Tests.Common/Build/Projects/KiotaProjectFileContents.cs
@@ -3,39 +3,37 @@
     public static class KiotaProjectFileContents
     {
         public const string NetCoreApp =
-            @"
-<Project Sdk=""Microsoft.NET.Sdk"">
-  <PropertyGroup>
-    <TargetFramework>net8.0</TargetFramework>
-  </PropertyGroup>
-  <ItemGroup>
-    <PackageReference Include=""Azure.Identity"" Version=""1.11.4"" />
-    <PackageReference Include=""Microsoft.Kiota.Abstractions"" Version=""1.9.1"" />
-    <PackageReference Include=""Microsoft.Kiota.Authentication.Azure"" Version=""1.1.5"" />
-    <PackageReference Include=""Microsoft.Kiota.Http.HttpClientLibrary"" Version=""1.4.2"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Form"" Version=""1.2.3"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Json"" Version=""1.3.2"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Text"" Version=""1.2.0"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Multipart"" Version=""1.1.5"" />
-  </ItemGroup>
-</Project>";
+            """
+            <Project Sdk=""Microsoft.NET.Sdk"">
+              <PropertyGroup>
+                <TargetFramework>net8.0</TargetFramework>
+              </PropertyGroup>
+              <ItemGroup>
+                  <PackageReference Include="Microsoft.Kiota.Abstractions" Version="1.9.7" />
+                  <PackageReference Include="Microsoft.Kiota.Http.HttpClientLibrary" Version="1.4.3" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Form" Version="1.2.5" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Json" Version="1.3.3" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Multipart" Version="1.1.5" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Text" Version="1.2.2" />
+              </ItemGroup>
+            </Project>
+            """;
 
         public const string NetStandardLibrary =
-          @"
-<Project Sdk=""Microsoft.NET.Sdk"">
-  <PropertyGroup>
-    <TargetFramework>netstandard2.1</TargetFramework>
-  </PropertyGroup>
-  <ItemGroup>
-    <PackageReference Include=""Azure.Identity"" Version=""1.11.4"" />
-    <PackageReference Include=""Microsoft.Kiota.Abstractions"" Version=""1.9.1"" />
-    <PackageReference Include=""Microsoft.Kiota.Authentication.Azure"" Version=""1.1.5"" />
-    <PackageReference Include=""Microsoft.Kiota.Http.HttpClientLibrary"" Version=""1.4.2"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Form"" Version=""1.2.3"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Json"" Version=""1.3.2"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Text"" Version=""1.2.0"" />
-    <PackageReference Include=""Microsoft.Kiota.Serialization.Multipart"" Version=""1.1.5"" />
-  </ItemGroup>
-</Project>";
+            """
+            <Project Sdk=""Microsoft.NET.Sdk"">
+              <PropertyGroup>
+                <TargetFramework>netstandard2.1</TargetFramework>
+              </PropertyGroup>
+              <ItemGroup>
+                  <PackageReference Include="Microsoft.Kiota.Abstractions" Version="1.9.7" />
+                  <PackageReference Include="Microsoft.Kiota.Http.HttpClientLibrary" Version="1.4.3" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Form" Version="1.2.5" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Json" Version="1.3.3" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Multipart" Version="1.1.5" />
+                  <PackageReference Include="Microsoft.Kiota.Serialization.Text" Version="1.2.2" />
+              </ItemGroup>
+            </Project>
+            """;
     }
 }
