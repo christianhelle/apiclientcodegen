f382093bba27ce4213d6fcf7b352f0a0ceb60860
Christian Helle <christian.helle@outlook.com>
2024-07-20 10:54:48 +0200

    Fix EnumGetDescriptionTests

--- src/Core/ApiClientCodeGen.Core.Tests/Extensions/EnumGetDescriptionTests.cs
+++ src/Core/ApiClientCodeGen.Core.Tests/Extensions/EnumGetDescriptionTests.cs
@@ -16,10 +16,11 @@ namespace ApiClientCodeGen.Core.Tests.Extensions
         [InlineData(OpenApiSupportedTargetFramework.NetStandard16, "netstandard1.6")]
         [InlineData(OpenApiSupportedTargetFramework.NetStandard20, "netstandard2.0")]
         [InlineData(OpenApiSupportedTargetFramework.NetStandard21, "netstandard2.1")]
-        [InlineData(OpenApiSupportedTargetFramework.NetCoreApp31, "netcoreapp3.1")]
         [InlineData(OpenApiSupportedTargetFramework.Net47, "net47")]
-        [InlineData(OpenApiSupportedTargetFramework.Net50, "net5.0")]
+        [InlineData(OpenApiSupportedTargetFramework.Net48, "net48")]
         [InlineData(OpenApiSupportedTargetFramework.Net60, "net6.0")]
+        [InlineData(OpenApiSupportedTargetFramework.Net70, "net7.0")]
+        [InlineData(OpenApiSupportedTargetFramework.Net80, "net8.0")]
         public void GetDescription_Returns_Description(
             OpenApiSupportedTargetFramework framework,
             string expected)
