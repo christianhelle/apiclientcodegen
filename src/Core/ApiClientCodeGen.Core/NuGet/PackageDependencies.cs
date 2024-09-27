f0810173ffe1213a3bb496399eb987035746bac9
Christian Helle <christian.helle@outlook.com>
2024-09-27 11:54:18 +0200

    Update Polly dependency to version 8.4.2 in package dependencies

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -56,7 +56,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency Polly =
             new PackageDependency(
                 "Polly",
-                "8.4.1");
+                "8.4.2");
 
         public static readonly PackageDependency AutoRestCSharp =
             new PackageDependency(
