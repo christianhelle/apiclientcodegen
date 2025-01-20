396486f4a0f005fc12087090d43d094d9f643a59
Christian Helle <christian.helle@outlook.com>
2025-01-20 17:51:52 +0100

    Update RefitterCommand description to version 1.5.0

--- src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
+++ src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/RefitterCommand.cs
@@ -6,7 +6,7 @@ using Rapicgen.Core.Options.Refitter;
 
 namespace Rapicgen.CLI.Commands.CSharp;
 
-[Command("refitter", Description = "Refitter (v1.4.1)")]
+[Command("refitter", Description = "Refitter (v1.5.0)")]
 public class RefitterCommand : CodeGeneratorCommand
 {
     private readonly IRefitterCodeGeneratorFactory factory;
