6555b6071ee4a3e4d32e286f18192d739ef29aa7
Christian Helle <christian.helle@outlook.com>
2025-05-03 09:38:16 +0200

    Update NSwag version to 14.4.0 in generating code and CLI description

--- src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/NswagCommand.cs
+++ src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/NswagCommand.cs
@@ -9,7 +9,7 @@ using Rapicgen.Core.Options.NSwag;
 
 namespace Rapicgen.CLI.Commands.CSharp
 {
-    [Command("nswag", Description = "NSwag (v14.3.0)")]
+    [Command("nswag", Description = "NSwag (v14.4.0)")]
     public class NSwagCommand : CodeGeneratorCommand, INSwagOptions
     {
         private readonly IOpenApiDocumentFactory openApiDocumentFactory;
