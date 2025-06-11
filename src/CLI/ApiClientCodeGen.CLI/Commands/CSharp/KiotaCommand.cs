5255428f88834d07d90256ec55a4491c1db7f051
Christian Helle <christian.helle@outlook.com>
2025-06-11 23:02:55 +0200

    Update Kiota version in CLI tool to v1.27.0

--- src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/KiotaCommand.cs
+++ src/CLI/ApiClientCodeGen.CLI/Commands/CSharp/KiotaCommand.cs
@@ -8,7 +8,7 @@ using Rapicgen.Core.Options.Kiota;
 
 namespace Rapicgen.CLI.Commands.CSharp;
 
-[Command("kiota", Description = "Microsoft Kiota (v1.26.1)")]
+[Command("kiota", Description = "Microsoft Kiota (v1.27.0)")]
 public class KiotaCommand : CodeGeneratorCommand, IKiotaOptions
 {
     private readonly IProcessLauncher processLauncher;
