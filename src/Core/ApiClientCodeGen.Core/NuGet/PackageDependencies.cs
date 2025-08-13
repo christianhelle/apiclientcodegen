e91984834110383a194697bd34920a3d701ce87f
Christian Helle <christian.helle@outlook.com>
2025-08-13 08:57:16 +0200

    chore(deps): update Azure.Core to version 1.47.2

--- src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
+++ src/Core/ApiClientCodeGen.Core/NuGet/PackageDependencies.cs
@@ -66,7 +66,7 @@ namespace Rapicgen.Core.NuGet
         public static readonly PackageDependency AzureCore =
             new PackageDependency(
                 "Azure.Core",
-                "1.47.1");
+                "1.47.2");
 
         public static readonly PackageDependency AzureIdentity =
             new PackageDependency(
