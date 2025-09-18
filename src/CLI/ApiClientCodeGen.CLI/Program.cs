8fe237a39887c7560cedf04871ce79db5cdfba5a
Christian Helle <christian.helle@outlook.com>
2025-09-18 09:01:28 +0200

    Update Refitter command description to version 1.6.3

--- src/CLI/ApiClientCodeGen.CLI/Program.cs
+++ src/CLI/ApiClientCodeGen.CLI/Program.cs
@@ -53,7 +53,7 @@ namespace Rapicgen.CLI
                             .WithExample(new[] { "nswag", "petstore.json", "GeneratedCode", "Output.cs" });
 
                         cs.AddCommand<RefitterCommand>("refitter")
-                            .WithDescription("Refitter (v1.6.2)")
+                            .WithDescription("Refitter (v1.6.3)")
                             .WithExample(new[] { "refitter", "petstore.json", "GeneratedCode", "Output.cs" });
 
                         cs.AddCommand<SwaggerCodegenCommand>("swagger")
