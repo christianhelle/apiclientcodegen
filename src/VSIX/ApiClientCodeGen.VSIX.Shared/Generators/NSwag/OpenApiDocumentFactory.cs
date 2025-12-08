7593777221650ebd85c68b43048d5e935f03833d
Christian Helle <christian.helle@outlook.com>
2025-12-08 15:03:22 +0100

    Fix file extension check

--- src/VSIX/ApiClientCodeGen.VSIX.Shared/Generators/NSwag/OpenApiDocumentFactory.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Shared/Generators/NSwag/OpenApiDocumentFactory.cs
@@ -15,13 +15,13 @@ namespace Rapicgen.Generators.NSwag
             try
             {
                 return await ThreadHelper.JoinableTaskFactory.RunAsync(
-                    () => swaggerFile.EndsWith("yaml") || swaggerFile.EndsWith("yml")
+                    () => swaggerFile.EndsWith(".yaml") || swaggerFile.EndsWith(".yml")
                         ? OpenApiYamlDocument.FromFileAsync(swaggerFile)
                         : OpenApiDocument.FromFileAsync(swaggerFile));
             }
             catch (NullReferenceException)
             {
-                return await (swaggerFile.EndsWith("yaml") || swaggerFile.EndsWith("yml")
+                return await (swaggerFile.EndsWith(".yaml") || swaggerFile.EndsWith(".yml")
                         ? OpenApiYamlDocument.FromFileAsync(swaggerFile)
                         : OpenApiDocument.FromFileAsync(swaggerFile));
             }
