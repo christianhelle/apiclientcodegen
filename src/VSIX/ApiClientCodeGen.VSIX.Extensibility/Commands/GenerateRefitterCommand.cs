744a878c75e51ead6dd5999056bfdf25d81f68e8
Christian Helle <christian.helle@outlook.com>
2025-12-26 14:11:44 +0100

    WIP

--- src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/GenerateRefitterCommand.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/GenerateRefitterCommand.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.VisualStudio.Extensibility;
 using Microsoft.VisualStudio.Extensibility.Commands;
+using Rapicgen.Core.Generators;
+using Rapicgen.Core.Generators.Refitter;
+using Rapicgen.Core.Installer;
+using Rapicgen.Core.Logging;
+using Rapicgen.Core.Options.Refitter;
+using System.Diagnostics;
 
 namespace ApiClientCodeGen.VSIX.Extensibility.Commands;
 
 [VisualStudioContribution]
-public class GenerateRefitterCommand : Command
+public class GenerateRefitterCommand(TraceSource traceSource) : Command
 {
     public override CommandConfiguration CommandConfiguration => new("%RefitterCommand.DisplayName%")
     {
@@ -12,8 +18,43 @@ public class GenerateRefitterCommand : Command
         VisibleWhen = ActivationConstraint.ClientContext(ClientContextKey.Shell.ActiveSelectionFileName, ".(json|ya?ml)")
     };
 
-    public override Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
     {
-        return Task.CompletedTask;
+        Logger.Instance.TrackFeatureUsage("Generate Refitter output");
+
+        var item = await context.GetSelectedPathAsync(cancellationToken);
+        var inputFile = item.AbsolutePath;
+
+        var csharpCode = await Task.Run(() => GenerateCode(inputFile));
+        if (csharpCode is not null)
+        {
+            await File.WriteAllTextAsync(
+                inputFile.Replace(new FileInfo(inputFile).Extension, "cs"),
+                csharpCode,
+                cancellationToken);
+        }
+    }
+
+    private string? GenerateCode(string inputFile)
+    {
+        try
+        {
+            var generator = new RefitterCodeGenerator(
+                inputFile,
+                default!,
+                new ProcessLauncher(),
+                new DependencyInstaller(
+                    new NpmInstaller(new ProcessLauncher()),
+                    new FileDownloader(new WebDownloader()),
+                    new ProcessLauncher()),
+                new DefaultRefitterOptions());
+
+            return generator.GenerateCode(null);
+        }
+        catch (Exception e)
+        {
+            traceSource.TraceEvent(TraceEventType.Error, 0, $"Error generating Refitter code: {e}");
+            return null;
+        }
     }
 }
---
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/IGenerateRefitterCommand.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+
+namespace ApiClientCodeGen.VSIX.Extensibility.Commands
+{
+    public interface IGenerateRefitterCommand
+    {
+        CommandConfiguration CommandConfiguration { get; }
+
+        Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken);
+    }
+}
