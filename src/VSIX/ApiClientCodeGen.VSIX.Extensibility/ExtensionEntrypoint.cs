b72bf5fe4a0a5fe860178fd33df0447a22d47259
Christian Helle <christian.helle@outlook.com>
2025-12-26 13:28:39 +0100

    Create VS extension projection using out-of-process extensibility model

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/ExtensionEntrypoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Microsoft;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+
+namespace ApiClientCodeGen.VSIX.Extensibility
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                id: "APIClientCodeGenerator2026.c28b8f61-bafa-4dc5-a0cc-44b47f3f1c39",
+                version: this.ExtensionAssemblyVersion,
+                publisherName: "ChristianResmaHelle",
+                displayName: "REST API Client Code Generator",
+                description: "Generate REST API client code from OpenAPI/Swagger specifications"),
+        };
+
+        public static CommandGroupConfiguration MyGroup => new()
+        {
+            Children = new[]
+            {
+                GroupChild.Command<Commands.GenerateRefitterCommand>(),
+                GroupChild.Command<Commands.GenerateNSwagCommand>(),
+                GroupChild.Command<Commands.GenerateOpenApiCommand>(),
+                GroupChild.Command<Commands.GenerateKiotaCommand>(),
+                GroupChild.Command<Commands.GenerateSwaggerCommand>(),
+                GroupChild.Command<Commands.GenerateAutoRestCommand>(),
+                GroupChild.Command<Commands.AboutCommand>(),
+            }            
+        };
+
+        [VisualStudioContribution]
+        public static MenuConfiguration MyMenu => new("%ApiClientCodeGenerator.GroupDisplayName%")
+        {
+            Placements = [CommandPlacement.KnownPlacements.ExtensionsMenu],
+            Children = [MenuChild.Group(MyGroup)],           
+        };
+    }
+}
