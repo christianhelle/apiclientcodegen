59e89c01a556bc86c09f81e5774a4b8d062a35cb
Christian Helle <christian.helle@outlook.com>
2025-12-28 13:44:12 +0100

    Add support for writing to Output Window

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/OutputWindow.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+
+namespace ApiClientCodeGen.VSIX.Extensibility;
+
+#pragma warning disable VSEXTPREVIEW_OUTPUTWINDOW // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+internal static class CommandExtensions
+{
+    public static async Task WriteToOutputWindowAsync(
+        this Command command,
+        string message,
+        CancellationToken cancellationToken)
+    {
+        var outputChannel = await command.Extensibility
+            .Views()
+            .Output
+            .CreateOutputChannelAsync(
+                "REST API Client Code Generator",
+                cancellationToken);
+
+        if (outputChannel != null)
+            await outputChannel.WriteLineAsync(message);
+    }
+}
