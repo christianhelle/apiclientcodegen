bca9325608ce424b14ad0a7621a3bba8fe446415
Christian Helle <christian.helle@outlook.com>
2025-12-28 18:07:54 +0100

    Add simple About dialog

--- src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/AboutCommand.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/AboutCommand.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.Extensibility;
+﻿using ApiClientCodeGen.VSIX.Extensibility.Dialogs;
+using Microsoft.VisualStudio.Extensibility;
 using Microsoft.VisualStudio.Extensibility.Commands;
 
 namespace ApiClientCodeGen.VSIX.Extensibility.Commands;
@@ -11,9 +12,17 @@ public class AboutCommand : Command
         Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
     };
 
-    public override Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
     {
-        // Show about dialog as a simple prompt
-        return Task.CompletedTask;
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        var dialog = new AboutDialog(
+            displayName: "REST API Client Code Generator",
+            description: "Generate REST API client code from OpenAPI/Swagger specifications",
+            version: "1.0.0",
+            publisher: "Christian Resma Helle",
+            extensionId: "Rapicgen");
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+        await Extensibility.Shell().ShowDialogAsync(dialog, cancellationToken);
     }
 }
