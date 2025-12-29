5d2b98cf716f1b11646f3279e0d075d7e9e3c2c3
Christian Helle <christian.helle@outlook.com>
2025-12-29 13:35:08 +0100

    Potential bugs with extension replacement and fragile index arithmetic.

--- src/VSIX/ApiClientCodeGen.VSIX.Extensibility/OutputFile.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/OutputFile.cs
@@ -5,12 +5,19 @@ public static class OutputFile
     public static string GetOutputFilename(string inputFile)
     {
         var fileInfo = new FileInfo(inputFile);
-        var outputFile = 
-            Path.Combine(
-                fileInfo.Directory!.FullName,
-                char.ToUpper(fileInfo.FullName[fileInfo.Directory.FullName.Length + 1]) +
-                fileInfo.FullName.Substring(fileInfo.Directory.FullName.Length + 2))
-            .Replace(fileInfo.Extension, ".cs");
+        var fileName = fileInfo.Name;
+        var directoryPath = fileInfo.DirectoryName
+            ?? Path.GetDirectoryName(inputFile)
+            ?? string.Empty;
+
+        // Capitalize the first character safely
+        var safeFileName = string.IsNullOrEmpty(fileName) 
+            ? string.Empty 
+            : char.ToUpper(fileName[0]) + fileName.Substring(1);
+
+        var outputFile = Path.ChangeExtension(
+            Path.Combine(directoryPath, safeFileName), 
+            ".cs");
 
         return outputFile;
     }
