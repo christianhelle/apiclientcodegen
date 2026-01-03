4b95f88a6e3cdabab031713a187d0562a3912255
Christian Helle <christian.helle@outlook.com>
2026-01-03 17:32:04 +0100

    Use actual text value for analytics description in About dialog

--- src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/AboutCommand.cs
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/Commands/AboutCommand.cs
@@ -24,7 +24,7 @@ public class AboutCommand : Command
             publisher: "Christian Resma Helle",
             extensionId: "f7530eb1-1ce9-46ac-8fab-165b68cf3d61",
             supportKey: SupportInformation.GetSupportKey(),
-            analyticsDescription: "%Settings.Analytics.Description%");
+            analyticsDescription: "This extension collects errors and tracks feature usages to Exceptionless and Azure Application Insights by default and you may opt-out of telemetry collection at any time.\n\nUser tracking is done anonymously using a Support Key (shown in the About dialog) and a generated anonymous identity based on a secure hash algorithm of username@host.\n\nOnly the following user actions are tracked:\n- Custom Tool executions\n- Clicking on \"New REST API Client\" from the Solution Explorer\n- Clicking on \"Generate NSwag Studio Output\" from the Solution Explorer\n\nThe purpose of all this is to know what features I should put the most effort in and for support scenarios where users are to specify their support key when reporting issues.");
 #pragma warning restore CA2000 // Dispose objects before losing scope
 
         await Extensibility.Shell().ShowDialogAsync(dialog, cancellationToken);
