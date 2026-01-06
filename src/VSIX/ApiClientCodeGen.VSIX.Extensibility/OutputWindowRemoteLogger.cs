cfdb9cb006c75ea8d5eb0c34a6e80d298303a0e3
Christian Helle <christian.helle@outlook.com>
2026-01-06 13:50:22 +0100

    Introduce OutputWindowRemoteLogger

---
+++ src/VSIX/ApiClientCodeGen.VSIX.Extensibility/OutputWindowRemoteLogger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.Extensibility.Documents;
+using Rapicgen.Core.Logging;
+
+namespace ApiClientCodeGen.VSIX.Extensibility;
+
+#pragma warning disable VSEXTPREVIEW_OUTPUTWINDOW // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+public class OutputWindowRemoteLogger(OutputChannel outputChannel) : IRemoteLogger
+{
+    public void Disable()
+    {
+        // Method intentionally left empty.
+    }
+
+    public void Enable()
+    {
+        // Method intentionally left empty.
+    }
+
+    public void TrackDependency(
+        string dependencyName,
+        string? data = null,
+        DateTimeOffset startTime = default,
+        TimeSpan duration = default,
+        bool success = false)
+    {
+        // Method intentionally left empty.
+    }
+
+    public void TrackError(Exception exception)
+    {
+        WriteLine(exception);
+    }
+
+    public void TrackFeatureUsage(string featureName, params string[] tags)
+    {
+        // Method intentionally left empty.
+    }
+
+    public void WriteLine(object data)
+    {
+        outputChannel.Writer.WriteLine(data);
+    }
+}
