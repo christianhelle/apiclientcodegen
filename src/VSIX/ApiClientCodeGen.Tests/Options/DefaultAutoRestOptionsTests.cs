61dca1eeee3eae96257ee735653fa6f94819026c
Christian Helle <christian.helle@outlook.com>
2026-03-22 13:09:26 +0100

    Mark VSIX AutoRest options tests as deprecated

--- src/VSIX/ApiClientCodeGen.Tests/Options/DefaultAutoRestOptionsTests.cs
+++ src/VSIX/ApiClientCodeGen.Tests/Options/DefaultAutoRestOptionsTests.cs
@@ -1,35 +1,8 @@
-﻿using Rapicgen.Core.Options.AutoRest;
-using FluentAssertions;
+using ApiClientCodeGen.Tests.Common;
 
 namespace Rapicgen.Tests.Options
 {
-    
-    public class DefaultAutoRestOptionsTests
+    public class DefaultAutoRestOptionsTests : AutoRestDeprecatedTestClass
     {
-        private readonly IAutoRestOptions sut = new DefaultAutoRestOptions();
-
-        [Xunit.Fact]
-        public void AddCredentials_Be_False()
-            => sut.AddCredentials.Should().BeFalse();
-
-        [Xunit.Fact]
-        public void SyncMethods_Be_Essential()
-            => sut.SyncMethods.Should().Be(SyncMethodOptions.Essential);
-
-        [Xunit.Fact]
-        public void ClientSideValidation_Be_False()
-            => sut.ClientSideValidation.Should().BeTrue();
-
-        [Xunit.Fact]
-        public void OverrideClientName_Be_False()
-            => sut.OverrideClientName.Should().BeFalse();
-
-        [Xunit.Fact]
-        public void UseInternalConstructors_Be_False()
-            => sut.UseInternalConstructors.Should().BeFalse();
-
-        [Xunit.Fact]
-        public void UseDateTimeOffset_Be_False()
-            => sut.UseDateTimeOffset.Should().BeFalse();
     }
 }
